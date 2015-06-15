9eba294d21eb69f81bb99a6bb9e496cb32082467
Zack Michael <zack@squishmedia.com>
2015-06-15 12:25:22 -0700

    updated cli and added colors

--- cli.js
+++ cli.js
@@ -1,13 +1,26 @@
 var validate = require('./index.js');
+var colors = require('colors/safe');
 
 module.exports = function () {
 	var args = process.argv;
 	var dir  = args[2];
 
 	if (dir) {
-	    validate.BIDSPath(dir, function (errors) {
-	        for (var i = 0; i < errors.length; i++) {
-	            console.log(errors[i]);
+	    validate.BIDSPath(dir, function (issues) {
+	        // for (var i = 0; i < issues.length; i++) {
+	        //     console.log(issues[i]);
+	        // }
+	        console.log();
+	        for (var i = 0; i < issues.length; i++) {
+	        	console.log('\t' + colors.red(issues[i].file.name));
+	        	for (var j = 0; j < issues[i].errors.length; j++) {
+	        		var error = issues[i].errors[j];
+	        		if (!error) {continue;}
+	        		console.log('\t' + error.reason);
+	        		console.log('\t@ line: ' + error.line + ' character: ' + error.character);
+	        		console.log('\t' + error.evidence);
+	        		console.log();
+	        	}
 	        }
 	    });
 	}
