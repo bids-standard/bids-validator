d0a7d78449c117f59b7c5638f21f03f29a161857
Zack Michael <zack@squishmedia.com>
2015-06-26 13:05:02 -0700

    fixed issues with using validator as a global cli

--- index.js
+++ index.js
@@ -4,6 +4,3 @@ var validate = require('./validators');
 // export validations for use in other applications
 module.exports = validate;
 
-// import and init command line interface
-require('./cli')();
-
