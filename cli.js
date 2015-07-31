df70c0ceffde5012d442a54f21903faa35f6e5e6
Zack Michael <zack@squishmedia.com>
2015-07-31 16:16:06 -0700

    Setup logging of warnings

--- cli.js
+++ cli.js
@@ -5,20 +5,25 @@ module.exports = function (args) {
 	var dir  = args[2];
 
 	if (dir) {
-	    validate.BIDS(dir, function (issues) {
+	    validate.BIDS(dir, function (errors, warnings) {
 	        console.log();
-	        for (var i = 0; i < issues.length; i++) {
-	        	console.log('\t' + colors.red(issues[i].file.name));
-	        	for (var j = 0; j < issues[i].errors.length; j++) {
-	        		var error = issues[i].errors[j];
-	        		if (!error) {continue;}
-	        		console.log('\t' + error.reason);
-	        		console.log('\t@ line: ' + error.line + ' character: ' + error.character);
-	        		console.log('\t' + error.evidence);
-	        		console.log('\t' + error.severity);
-	        		console.log();
-	        	}
-	        }
+	        logIssues(errors, 'red');
+	        logIssues(warnings, 'yellow');
 	    });
 	}
 };
+
+function logIssues (issues, color) {
+	for (var i = 0; i < issues.length; i++) {
+    	console.log('\t' + colors[color](issues[i].file.name));
+    	for (var j = 0; j < issues[i].errors.length; j++) {
+    		var error = issues[i].errors[j];
+    		if (!error) {continue;}
+    		console.log('\t' + error.reason);
+    		console.log('\t@ line: ' + error.line + ' character: ' + error.character);
+    		console.log('\t' + error.evidence);
+    		console.log('\t' + error.severity);
+    		console.log();
+    	}
+    }
+}
