40e5a00cb53c0be33ee668fa824b2cc35721ad42
Zack Michael <zack@squishmedia.com>
2015-09-21 16:27:04 -0700

    updated JSON tests to account for sidecar boolean

--- tests/json.spec.js
+++ tests/json.spec.js
@@ -4,22 +4,22 @@ var validate = require('../index');
 describe('JSON', function(){
 	
 	it('should catch missing closing brackets', function(){
-		validate.JSON('{', function (errors) {
+		validate.JSON('{', false, function (errors) {
 			assert(errors && errors.length > 0);
 		});
 	});
 
-	it('should have key/value pair for "repetition_time"', function(){
-		var jsonObj = '{"repetition_time": 0.5, "echo_time": 0.005, "flip_angle": 90}';
-		validate.JSON(jsonObj, function (errors) {
+	it('sidecars should have key/value pair for "RepetitionTime"', function(){
+		var jsonObj = '{"RepetitionTime": 0.5, "echo_time": 0.005, "flip_angle": 90}';
+		validate.JSON(jsonObj, true, function (errors) {
 			assert(errors == null);
 		});
 		var jsonObjInval = '{"echo_time": 0.005, "flip_angle": 90}';
-		validate.JSON(jsonObjInval, function (errors) {
+		validate.JSON(jsonObjInval, true, function (errors) {
 			assert(errors && errors.length === 1);
 		});
-		var jsonObjSpell = '{"repitition_time": 0.5, "echo_time": 0.005, "flip_angle": 90}';
-		validate.JSON(jsonObjSpell, function (errors) {
+		var jsonObjSpell = '{"RepititionTime": 0.5, "echo_time": 0.005, "flip_angle": 90}';
+		validate.JSON(jsonObjSpell, true, function (errors) {
 			assert(errors && errors.length === 1);
 		});
 	});
