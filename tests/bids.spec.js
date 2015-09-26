e7947f7f1b3a74de1f1318b037a7ab93d59327a0
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-09-25 20:51:45 -0700

    added BIDS example tests

--- tests/bids.spec.js
+++ tests/bids.spec.js
@@ -1,5 +1,19 @@
-var assert = require('assert');
+var assert = require('chai').assert
+var validate = require('../index.js');
 var BIDS   = require('../validators/bids');
+var request = require('sync-request');
+var fs = require('fs');
+var AdmZip = require('adm-zip');
+var path = require('path');
+var Test = require("mocha/lib/test");
+
+function getDirectories(srcpath) {
+    return fs.readdirSync(srcpath).filter(function(file) {
+        return fs.statSync(path.join(srcpath, file)).isDirectory();
+    });
+}
+
+
 
 describe('BIDS', function(){
 
@@ -20,4 +34,34 @@ describe('BIDS', function(){
         });
     });
 
+});
+var suite = describe('BIDS examples - ', function() {
+    this.timeout(100000);
+
+    before(function() {
+        if (!fs.existsSync("tests/data")) {
+            console.log('downloading test data')
+            response = request("GET", "http://github.com/INCF/BIDS-examples/archive/1.0.0-rc1.zip")
+            fs.mkdirSync("tests/data")
+            fs.writeFileSync("tests/data/examples.zip", response.body)
+            var zip = new AdmZip("tests/data/examples.zip");
+            console.log('unzipping test data')
+            zip.extractAllTo("tests/data/", true);
+        }
+
+        datasetDirectories = getDirectories("tests/data/BIDS-examples-1.0.0-rc1/")
+
+        datasetDirectories.forEach(function testDataset(path){
+            suite.addTest(new Test(path, function (){
+                validate.BIDS("tests/data/BIDS-examples-1.0.0-rc1/" + path, function (errors, warnings) {
+                    assert.equal(errors, []);
+                    assert.equal(warnings, []);
+                });
+            }));
+        });
+    });
+
+    return it('should be ok', function() {
+        return 0;
+    });
 });
