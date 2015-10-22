604e44aa1bb3a852779f524361f66f7231cd670c
Zack Michael <zack@squishmedia.com>
2015-10-22 13:10:51 -0700

    Moved merged dict generation to it's own function and updated some naming conventions

--- validators/nii.js
+++ validators/nii.js
@@ -1,5 +1,6 @@
-var async  = require('async');
-var Issue = require('../utils').Issue;
+var async = require('async');
+var utils = require('../utils');
+var Issue = utils.Issue;
 
 /**
  * NIFTI
@@ -12,101 +13,89 @@ var Issue = require('../utils').Issue;
 module.exports = function NIFTI (path, jsonContentsDict, callback) {
     var errors = [];
     var warnings = [];
-    var potentialJSONs = determinePotentialSidecars(path);
+    var potentialSidecars = determinePotentialSidecars(path);
+    var mergedDictionary = generateMergedSidecarDict(potentialSidecars, jsonContentsDict);
+    var locationMessage = "It can be included one of the following locations: " + potentialSidecars.join(", ");
 
-    var mergedDictionary = {};
-    async.forEachOf(potentialJSONs, function (file, key, cb) {
-        var jsObj = jsonContentsDict[file];
-        if (jsObj) {
-            for (var attrname in jsObj) {
-                mergedDictionary[attrname] = jsObj[attrname];
-            }
+    if (path.endsWith("_bold.nii.gz") || path.endsWith("_sbref.nii.gz") || path.endsWith("_dwi.nii.gz")) {
+        if (!mergedDictionary.hasOwnProperty('EchoTime')) {
+            warnings.push(new Issue({
+                severity: "warning",
+                reason: "You should should define 'EchoTime' for this file. If you don't provide this information field map correction will not be possible. " + locationMessage
+            }));
         }
-        return cb();
-    }, function(){
-        var locMSg = "It can be included one of the following locations: " + potentialJSONs.join(", ");
-        if (path.endsWith("_bold.nii.gz") || path.endsWith("_sbref.nii.gz") || path.endsWith("_dwi.nii.gz")) {
-            if (!mergedDictionary.hasOwnProperty('EchoTime')) {
-                warnings.push(new Issue({
-                    severity: "warning",
-                    reason: "You should should define 'EchoTime' for this file. If you don't provide this information field map correction will not be possible. " + locMSg
-                }));
-            }
-            if (!mergedDictionary.hasOwnProperty('PhaseEncodingDirection')) {
-                warnings.push(new Issue({
-                    severity: "warning",
-                    reason: "You should should define 'PhaseEncodingDirection' for this file. If you don't provide this information field map correction will not be possible. " + locMSg
-                }));
-            }
-            if (!mergedDictionary.hasOwnProperty('EffectiveEchoSpacing')) {
-                warnings.push(new Issue({
-                    severity: "warning",
-                    reason: "You should should define 'EffectiveEchoSpacing' for this file. If you don't provide this information field map correction will not be possible. " + locMSg
-                }));
-            }
+        if (!mergedDictionary.hasOwnProperty('PhaseEncodingDirection')) {
+            warnings.push(new Issue({
+                severity: "warning",
+                reason: "You should should define 'PhaseEncodingDirection' for this file. If you don't provide this information field map correction will not be possible. " + locationMessage
+            }));
         }
-        if (path.endsWith("_dwi.nii.gz")) {
-            if (!mergedDictionary.hasOwnProperty('TotalReadoutTime')) {
-                warnings.push(new Issue({
-                    severity: "warning",
-                    reason: "You should should define 'TotalReadoutTime' for this file. If you don't provide this information field map correction using TOPUP might not be possible. " + locMSg
-                }));
-            }
+        if (!mergedDictionary.hasOwnProperty('EffectiveEchoSpacing')) {
+            warnings.push(new Issue({
+                severity: "warning",
+                reason: "You should should define 'EffectiveEchoSpacing' for this file. If you don't provide this information field map correction will not be possible. " + locationMessage
+            }));
         }
-        // we don't need slice timing or repetition time for SBref
-        if (path.endsWith("_bold.nii.gz")) {
-            if (!mergedDictionary.hasOwnProperty('RepetitionTime')) {
-                errors.push(new Issue({
-                    reason: "You have to define 'RepetitionTime' for this file. " + locMSg
-                }));
-            }
-            if (!mergedDictionary.hasOwnProperty('SliceTiming')) {
-                warnings.push(new Issue({
-                    severity: "warning",
-                    reason: "You should should define 'SliceTiming' for this file. If you don't provide this information slice time correction will not be possible. " + locMSg
-                }));
-            }
-            if (!mergedDictionary.hasOwnProperty('SliceEncodingDirection')) {
-                warnings.push(new Issue({
-                    severity: "warning",
-                    reason: "You should should define 'SliceEncodingDirection' for this file. If you don't provide this information slice time correction will not be possible. " + locMSg
-                }));
-            }
+    }
+    if (path.endsWith("_dwi.nii.gz")) {
+        if (!mergedDictionary.hasOwnProperty('TotalReadoutTime')) {
+            warnings.push(new Issue({
+                severity: "warning",
+                reason: "You should should define 'TotalReadoutTime' for this file. If you don't provide this information field map correction using TOPUP might not be possible. " + locationMessage
+            }));
         }
-        else if (path.endsWith("_phasediff.nii.gz")){
-            if (!mergedDictionary.hasOwnProperty('EchoTimeDifference')) {
-                errors.push(new Issue({
-                    reason: "You have to define 'EchoTimeDifference' for this file. " + locMSg
-                }));
-            }
-        } else if (path.endsWith("_phase1.nii.gz") || path.endsWith("_phase2.nii.gz")){
-            if (!mergedDictionary.hasOwnProperty('EchoTime')) {
-                errors.push(new Issue({
-                    reason: "You have to define 'EchoTime' for this file. " + locMSg
-                }));
-            }
-        } else if (path.endsWith("_fieldmap.nii.gz")){
-            if (!mergedDictionary.hasOwnProperty('Units')) {
-                errors.push(new Issue({
-                    reason: "You have to define 'Units' for this file. " + locMSg
-                }));
-            }
-        } else if (path.endsWith("_epi.nii.gz")){
-            if (!mergedDictionary.hasOwnProperty('PhaseEncodingDirection')) {
-                errors.push(new Issue({
-                    reason: "You have to define 'PhaseEncodingDirection' for this file. " + locMSg
-                }));
-            }
-            if (!mergedDictionary.hasOwnProperty('TotalReadoutTime')) {
-                errors.push(new Issue({
-                    reason: "You have to define 'TotalReadoutTime' for this file. " + locMSg
-                }));
-            }
+    }
+    // we don't need slice timing or repetition time for SBref
+    if (path.endsWith("_bold.nii.gz")) {
+        if (!mergedDictionary.hasOwnProperty('RepetitionTime')) {
+            errors.push(new Issue({
+                reason: "You have to define 'RepetitionTime' for this file. " + locationMessage
+            }));
         }
-        callback(errors, warnings);
-    });
-
-
+        if (!mergedDictionary.hasOwnProperty('SliceTiming')) {
+            warnings.push(new Issue({
+                severity: "warning",
+                reason: "You should should define 'SliceTiming' for this file. If you don't provide this information slice time correction will not be possible. " + locationMessage
+            }));
+        }
+        if (!mergedDictionary.hasOwnProperty('SliceEncodingDirection')) {
+            warnings.push(new Issue({
+                severity: "warning",
+                reason: "You should should define 'SliceEncodingDirection' for this file. If you don't provide this information slice time correction will not be possible. " + locationMessage
+            }));
+        }
+    }
+    else if (path.endsWith("_phasediff.nii.gz")){
+        if (!mergedDictionary.hasOwnProperty('EchoTimeDifference')) {
+            errors.push(new Issue({
+                reason: "You have to define 'EchoTimeDifference' for this file. " + locationMessage
+            }));
+        }
+    } else if (path.endsWith("_phase1.nii.gz") || path.endsWith("_phase2.nii.gz")){
+        if (!mergedDictionary.hasOwnProperty('EchoTime')) {
+            errors.push(new Issue({
+                reason: "You have to define 'EchoTime' for this file. " + locationMessage
+            }));
+        }
+    } else if (path.endsWith("_fieldmap.nii.gz")){
+        if (!mergedDictionary.hasOwnProperty('Units')) {
+            errors.push(new Issue({
+                reason: "You have to define 'Units' for this file. " + locationMessage
+            }));
+        }
+    } else if (path.endsWith("_epi.nii.gz")){
+        if (!mergedDictionary.hasOwnProperty('PhaseEncodingDirection')) {
+            errors.push(new Issue({
+                reason: "You have to define 'PhaseEncodingDirection' for this file. " + locationMessage
+            }));
+        }
+        if (!mergedDictionary.hasOwnProperty('TotalReadoutTime')) {
+            errors.push(new Issue({
+                reason: "You have to define 'TotalReadoutTime' for this file. " + locationMessage
+            }));
+        }
+    }
+    callback(errors, warnings);
 };
 
 
@@ -158,4 +147,27 @@ function determinePotentialSidecars(scanPath) {
     potentialJSONs.push(topLevelJSON);
 
     return potentialJSONs;
+}
+
+/**
+ * Generate Merged Sidecar Dictionary
+ *
+ * Takes an array of potential sidecards and a
+ * master object dictionary of all JSON file
+ * content and returns a merged dictionary
+ * containing all values from the potential
+ * sidecars.
+ */
+function generateMergedSidecarDict(potentialSidecars, jsonContents) {
+    var mergedDictionary = {};
+    for (var i = 0; i < potentialSidecars.length; i++) {
+        var sidecarName = potentialSidecars[i];
+        var jsonObject = jsonContents[sidecarName];
+        if (jsonObject) {
+            for (var key in jsonObject) {
+                mergedDictionary[key] = jsonObject[key];
+            }
+        }
+    }
+    return mergedDictionary;
 }
