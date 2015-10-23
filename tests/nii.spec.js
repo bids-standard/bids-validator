dfb5c348a0253413db8e788b522ff9da86890c64
Zack Michael <zack@squishmedia.com>
2015-10-23 15:59:11 -0700

    Began writing nifti validation tests

--- tests/nii.spec.js
+++ tests/nii.spec.js
@@ -3,4 +3,15 @@ var validate = require('../index');
 
 describe('NIFTI', function(){
 
+	var path = '';
+	var header = {error: 'Unable to read ' + path};
+	var jsonContentsDict = {};
+
+	it('should catch NIfTI file reading errors', function(){
+		validate.NIFTI(header, path, jsonContentsDict, function (errors, warnings) {
+			assert(errors && errors.length > 0);
+		});
+	});
+
+
 });
