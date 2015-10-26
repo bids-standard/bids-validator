5adb8b27771f79ea7f25a2deac129cd02d343d70
Zack Michael <zack@squishmedia.com>
2015-10-26 14:31:14 -0700

    Built out basics for testing for missing events files

--- tests/nii.spec.js
+++ tests/nii.spec.js
@@ -6,9 +6,10 @@ describe('NIFTI', function(){
 	var path = '';
 	var header = {error: 'Unable to read ' + path};
 	var jsonContentsDict = {};
+	var events = [];
 
 	it('should catch NIfTI file reading errors', function(){
-		validate.NIFTI(header, path, jsonContentsDict, function (errors, warnings) {
+		validate.NIFTI(header, path, jsonContentsDict, events, function (errors, warnings) {
 			assert(errors && errors.length > 0);
 		});
 	});
