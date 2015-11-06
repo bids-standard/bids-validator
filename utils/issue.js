64c405e573c1edc1ccf1324c252aafb0952c7fe3
Zack Michael <zack@squishymedia.com>
2015-11-06 13:25:12 -0800

    fixed missing file path

--- utils/issue.js
+++ utils/issue.js
@@ -7,13 +7,13 @@ module.exports = function (options) {
 	var code = options.hasOwnProperty('code') ? options.code : null;
 	var issue = issues[code];
 
-	this.filename  = options.hasOwnProperty('file')      ? options.file.name         : null;
-	this.path      = options.hasOwnProperty('path')      ? options.file.relativePath : null;
-	this.evidence  = options.hasOwnProperty('evidence')  ? options.evidence          : null;
-    this.line      = options.hasOwnProperty('line')      ? options.line              : null;
-    this.character = options.hasOwnProperty('character') ? options.character         : null;
-    this.severity  = options.hasOwnProperty('severity')  ? options.severity          : issue.severity;
-    this.reason    = options.hasOwnProperty('reason')    ? options.reason            : issue.reason;
+	this.filename  = options.hasOwnProperty('file')      ? options.file.name : null;
+	this.path      = options.hasOwnProperty('path')      ? options.path      : options.file.relativePath;
+	this.evidence  = options.hasOwnProperty('evidence')  ? options.evidence  : null;
+    this.line      = options.hasOwnProperty('line')      ? options.line      : null;
+    this.character = options.hasOwnProperty('character') ? options.character : null;
+    this.severity  = options.hasOwnProperty('severity')  ? options.severity  : issue.severity;
+    this.reason    = options.hasOwnProperty('reason')    ? options.reason    : issue.reason;
 };
 
 var issues = {
