4ea56f0e62500b928d5955821e8126bf710bd103
Zack Michael <zack@squishymedia.com>
2016-02-01 09:40:24 -0800

    Fixed typo 'exits' to 'exist'

--- cli.js
+++ cli.js
@@ -16,7 +16,7 @@ module.exports = function (dir, options) {
 			}
 	    });
 	} else {
-		console.log(colors.red(dir + " does not exits"))
+		console.log(colors.red(dir + " does not exist"));
 	}
 };
 
