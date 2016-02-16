fabe1e4c7b262889b2f5d1ec9973ec1033921f3d
Zack Michael <zack@squishymedia.com>
2016-02-16 12:43:03 -0800

    Removed white space and added missing 'all_files' variable declaration

--- validators/session.js
+++ validators/session.js
@@ -4,7 +4,7 @@ var utils  = require('../utils');
  * session
  *
  * Takes a list of files and creates a set of file names that occur in subject
- * directories. Then generates a warning if a given subject is missing any 
+ * directories. Then generates a warning if a given subject is missing any
  * files from the set.
  */
 var session = function missingSessionFiles(fileList) {
@@ -13,11 +13,11 @@ var session = function missingSessionFiles(fileList) {
     for (var key in fileList) {
         var file = fileList[key];
         var filename;
-        
+
         if (!file || (typeof window != 'undefined' && !file.webkitRelativePath)) {
             continue;
         }
-         
+
         var path = utils.files.relativePath(file);
         if (!utils.type.isBIDS(path)) {
             continue;
@@ -34,20 +34,20 @@ var session = function missingSessionFiles(fileList) {
         if (typeof(subjects[subject]) === 'undefined') {
             subjects[subject] = [];
         }
-        // files are prepended with subject name, the following two commands 
+        // files are prepended with subject name, the following two commands
         // remove the subject from the file name to allow filenames to be more
         // easily compared
         filename = path.substring(path.match(subject).index + subject.length);
         filename = filename.replace(subject, '<sub>');
         subjects[subject].push(filename);
     }
-      
+
     var subject_files = [];
     for (var subject in subjects) {
         subject_files = subject_files.concat(subjects[subject])
     }
     // Converting to a Set removes all duplicates
-    all_files = new Set(subject_files);
+    var all_files = new Set(subject_files);
 
     // Converting it back to an array for easy iteration
     all_files = Array.from(all_files);
@@ -61,7 +61,7 @@ var session = function missingSessionFiles(fileList) {
                     evidence: "Subject: " + subject + "; Missing file: " + filename,
                     code: 38
                 }));
-            } 
+            }
         }
     }
     return issues;
