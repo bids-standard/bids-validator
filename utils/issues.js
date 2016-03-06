8853a6e0144c1513b7e5f859a2a4f45f790df7d1
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-03-05 18:33:17 -0800

    Make missing events file warning more explicit.

--- utils/issues.js
+++ utils/issues.js
@@ -100,7 +100,7 @@ module.exports = {
 	},
 	25: {
 		severity: 'warning',
-		reason:   'Task scans should have a correspondings events.tsv file.'
+		reason:   'Task scans should have a correspondings events.tsv file. If this is a resting state scan you can ignore this warning or rename the task to include the work "rest".'
 	},
 	26: {
 		severity: 'error',
