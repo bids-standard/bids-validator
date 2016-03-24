a47a5b45fe7fc7ce4463adac6e9522156554b675
Zack Michael <zack@squishymedia.com>
2016-03-24 13:47:54 -0700

    Fixed relativePath formation

--- validators/session.js
+++ validators/session.js
@@ -56,10 +56,9 @@ var session = function missingSessionFiles(fileList) {
     for (var subject in subjects) {
         for (var set_file in subject_files) {
             if (subjects[subject].indexOf(subject_files[set_file]) === -1) {
-                filename = subject_files[set_file].replace('<sub>', subject);
-                file.relativePath = '/' + subject + filename;
+                var relativePath = '/' + subject + subject_files[set_file].replace('<sub>', subject);
                 issues.push(new utils.Issue({
-                    file: {relativePath: file},
+                    file: {relativePath: relativePath},
                     evidence: "Subject: " + subject + "; Missing file: " + filename,
                     code: 38
                 }));
