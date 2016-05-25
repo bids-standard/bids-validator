b13d60b668f100022c9d4d6c51fc796e4d8a482a
Zack Michael <zack@squishymedia.com>
2016-05-25 13:22:24 -0700

    Change does not exit exit code to more specific error code 2

--- cli.js
+++ cli.js
@@ -24,7 +24,7 @@ module.exports = function (dir, options) {
         });
     } else {
         console.log(colors.red(dir + " does not exist"));
-        process.exit(1);
+        process.exit(2);
     }
 };
 
