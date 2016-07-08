fbe20f5b7825498e665efab4f73ba53b8ca82763
Zack Michael <zack@squishymedia.com>
2016-07-08 16:43:00 -0700

    Added more checks to bval/bvec files
    Ensured rows have the same number of values. Ensured values are numeric
    while also catching incorrect delimiters.

--- validators/bval.js
+++ validators/bval.js
@@ -1,4 +1,5 @@
 var Issue = require('../utils').Issue;
+var type  = require('../utils').type;
 
 /**
  * bval
@@ -15,6 +16,21 @@ module.exports = function bval (file, contents, callback) {
     if (contents.replace(/^\s+|\s+$/g, '').split('\n').length !== 1) {
         issues.push(new Issue({
             code: 30,
+            file: file
+        }));
+    }
+
+    var row = contents.replace(/^\s+|\s+$/g, '').split(' ');
+    var invalidValue = false;
+    for (var j = 0; j < row.length; j++) {
+        var value = row[j];
+        if (!type.isNumber(value)) {
+            invalidValue = true;
+        }
+    }
+    if (invalidValue) {
+        issues.push(new Issue({
+            code: 47,
             file: file
         }));
     }
