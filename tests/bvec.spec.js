0dab4b4602226fb80fa99b567bd9f54a4bcc0071
Zack Michael <zack@squishymedia.com>
2016-07-08 16:44:42 -0700

    Updated for new bvec checks

--- tests/bvec.spec.js
+++ tests/bvec.spec.js
@@ -3,15 +3,15 @@ var validate = require('../index');
 
 describe('bvec', function(){
 
-	var bval = '0 4 3 6 1 6 2 4 1\n 4 3 5 2 4 2 4 5';
+	var bvec = '0 4 3 6 1 6 2 4\n 4 3 5 2 4 2 4 5';
 
 	it('should not allow more or less than 3 rows', function () {
-		validate.bvec({}, bval, function (issues) {
+		validate.bvec({}, bvec, function (issues) {
 			assert(issues.length == 1 && issues[0].code == 31);
 		});
 
-		bval = '4 6 2 5\n 3 2 3 5\n 6 4 3 5';
-		validate.bvec({}, bval, function (issues) {
+		bvec = '4 6 2 5\n3 2 3 5\n6 4 3 5';
+		validate.bvec({}, bvec, function (issues) {
 			assert.deepEqual(issues, []);
 		});
 	});
