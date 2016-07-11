bd57e259facc19c5b30bb2fc105d49be1a58f022
Zack Michael <zack@squishymedia.com>
2016-07-11 10:12:22 -0700

    Updated b-file tests to match new separator/value type checks

--- tests/bval.spec.js
+++ tests/bval.spec.js
@@ -3,16 +3,24 @@ var validate = require('../index');
 
 describe('bval', function(){
 
-	var bval = '0 4 3 6 1 6 2 4 1\n 4 3 5 2 4 2 4 5';
+	it('should allow proper bval contents', function () {
+		var bval = '4 6 2 5 3 23 5';
+		validate.bval({}, bval, function (issues) {
+			assert.deepEqual(issues, []);
+		});
+	});
 
-	it('should not allow more or less than one row', function () {
+	it('should not allow more than one row', function () {
+		var bval = '0 4 3 6 1 6 2 4 1\n 4 3 5 2 4 2 4 5';
 		validate.bval({}, bval, function (issues) {
 			assert(issues.length == 1 && issues[0].code == 30);
 		});
+	});
 
-		bval = '4 6 2 5 3 23 5';
+	it('should catch doublespace separators', function () {
+		var bval = '4  6  2  5  3  23  5';
 		validate.bval({}, bval, function (issues) {
-			assert.deepEqual(issues, []);
+            assert(issues.length == 1 && issues[0].code == 47);
 		});
 	});
 
--- tests/bvec.spec.js
+++ tests/bvec.spec.js
@@ -3,17 +3,30 @@ var validate = require('../index');
 
 describe('bvec', function(){
 
-	var bvec = '0 4 3 6 1 6 2 4\n 4 3 5 2 4 2 4 5';
+    it('should allow valid bvec contents', function () {
+        var bvec = '4 6 2 5\n3 2 3 5\n6 4 3 5';
+        validate.bvec({}, bvec, function (issues) {
+            assert.deepEqual(issues, []);
+        });
+    });
 
 	it('should not allow more or less than 3 rows', function () {
+        var bvec = '0 4 3 6 1 6 2 4\n 4 3 5 2 4 2 4 5';
 		validate.bvec({}, bvec, function (issues) {
 			assert(issues.length == 1 && issues[0].code == 31);
 		});
 
-		bvec = '4 6 2 5\n3 2 3 5\n6 4 3 5';
-		validate.bvec({}, bvec, function (issues) {
-			assert.deepEqual(issues, []);
-		});
+        bvec = '0 4 3 6 1 6 2 4\n 4 3 5 2 4 2 4 5\n 4 3 5 2 4 2 4 5\n 4 3 5 2 4 2 4 5';
+        validate.bvec({}, bvec, function (issues) {
+            assert(issues.length == 1 && issues[0].code == 31);
+        });
 	});
+
+    it('should catch doublespace separators', function () {
+        var bvec = '4  6  2  5\n3  2  3  5\n6  4  3  5';
+        validate.bvec({}, bvec, function (issues) {
+            assert(issues.length == 1 && issues[0].code == 47);
+        });
+    });
 
 });
