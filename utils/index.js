3aefb4677332fe63d1028ece1874385a51648a57
Zack Michael <zack@squishymedia.com>
2016-07-29 15:10:55 -0700

    Added a utility function for checking array equality

--- utils/index.js
+++ utils/index.js
@@ -1,12 +1,14 @@
 require('./prototype');
-var type    = require('./type');
-var files   = require('./files');
-var Issue   = require('./issue');
-var issues  = require('./issues');
+var array  = require('./array');
+var files  = require('./files');
+var Issue  = require('./issue');
+var issues = require('./issues');
+var type   = require('./type');
 
 module.exports = {
-	files:   files,
-	type:    type,
-	Issue:   Issue,
-	issues:  issues
+	array:  array,
+	files:  files,
+	Issue:  Issue,
+	issues: issues,
+	type:   type,
 };
