2f1ca69f7df560833803444543c20e85609b1109
Zack Michael <zack@squishymedia.com>
2016-07-29 09:44:30 -0700

    Cleaned up tsv tests and added participants specific checks

--- tests/tsv.spec.js
+++ tests/tsv.spec.js
@@ -3,6 +3,8 @@ var validate = require('../index');
 
 describe('TSV', function(){
 
+// general tsv checks ------------------------------------------------------------------
+
     var file = {
         name: 'sub-08_ses-test_task-linebisection_events.tsv',
         relativePath: '/sub-08/ses-test/func/sub-08_ses-test_task-linebisection_events.tsv'
@@ -10,16 +12,16 @@ describe('TSV', function(){
 
     it('should not allow empty values saved as empty cells.', function () {
         var tsv = "1.0\t\t0.2\tresponse 1\t12.32";
-        validate.TSV(file, tsv, false, function (errors) {
-            assert(errors && errors.length > 0);
+        validate.TSV(file, tsv, function (issues) {
+            assert(issues.length === 1 && issues[0].code === 23);
         });
 
     });
 
     it('should not allow missing values that are specified by something other than "n/a".', function () {
         var tsv = "1.0\tNA\t0.2\tresponse 1\t12.32";
-        validate.TSV(file, tsv, false, function (errors) {
-            assert(errors && errors.length > 0);
+        validate.TSV(file, tsv, function (issues) {
+            assert(issues.length === 1 && issues[0].code === 24);
         });
     });
 
@@ -27,16 +29,62 @@ describe('TSV', function(){
         var tsv = 'header-one\theader-two\theader-three\n' +
                   'value-one\tvalue-two\n' +
                   'value-one\tvalue-two\tvalue-three';
-        validate.TSV(file, tsv, false, function (errors) {
-            assert(errors && errors.length > 0);
+        validate.TSV(file, tsv, function (issues) {
+            assert(issues.length === 1 && issues[0].code === 22);
         });
     });
 
-    it('require events files to have "onset" and "duration" columns', function () {
-        var tsv = 'header-one\theader-two\t4eader-three\n' +
+// events checks -----------------------------------------------------------------------
+
+    var eventsFile = {
+        name: 'events.tsv',
+        relativePath: '/events.tsv'
+    };
+
+    it('should require events files to have "onset" as first header', function () {
+        var tsv = 'header-one\tduration\t4eader-three\n' +
                   'value-one\tvalue-two\tvalue-three';
-        validate.TSV(file, tsv, true, function (errors) {
-            assert(errors && errors.length > 0);
+        validate.TSV(eventsFile, tsv, function (issues) {
+            assert(issues.length === 1 && issues[0].code === 20);
+        });
+    });
+
+    it('should require events files to have "duration" as second header', function () {
+        var tsv = 'onset\theader-two\t4eader-three\n' +
+                  'value-one\tvalue-two\tvalue-three';
+        validate.TSV(eventsFile, tsv, function (issues) {
+            assert(issues.length === 1 && issues[0].code === 21);
+        });
+    });
+
+    it('should not throw issues for a valid events file', function () {
+        var tsv = 'onset\tduration\t4eader-three\n' +
+                  'value-one\tvalue-two\tvalue-three';
+        validate.TSV(eventsFile, tsv, function (issues) {
+            assert.deepEqual(issues, []);
+        });
+    });
+
+// participants checks -----------------------------------------------------------------
+
+    var participantsFile = {
+        name: 'participants.tsv',
+        relativePath: '/participants.tsv'
+    };
+
+    it("should not allow participants.tsv files without participant_id columns", function () {
+        var tsv = 'subject_id\theader-two\t4eader-three\n' +
+                  'value-one\tvalue-two\tvalue-three';
+        validate.TSV(participantsFile, tsv, function (issues) {
+            assert(issues.length === 1 && issues[0].code === 48);
+        });
+    });
+
+    it("should allow a valid participants.tsv file", function () {
+        var tsv = 'participant_id\theader-two\t4eader-three\n' +
+                  'value-one\tvalue-two\tvalue-three';
+        validate.TSV(participantsFile, tsv, function (issues) {
+            assert.deepEqual(issues, []);
         });
     });
 
