c1c2108916ea48f8da93447e3d7f811c9efd395a
zack <zack@squishymedia.com>
2016-09-30 14:54:24 -0700

    Separated out basic json validation for reusability

--- utils/index.js
+++ utils/index.js
@@ -2,6 +2,7 @@ require('./prototype');
 var array      = require('./array');
 var files      = require('./files');
 var issues     = require('./issues');
+var json       = require('./json');
 var modalities = require('./modalities');
 var options    = require('./options');
 var type       = require('./type');
@@ -10,6 +11,7 @@ module.exports = {
 	array:      array,
 	files:      files,
 	issues:     issues,
+	json:       json,
 	modalities: modalities,
 	options:    options,
 	type:       type
