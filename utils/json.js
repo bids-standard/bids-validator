7f927cbd844bc5e1d19896b8c6a1a1cf0c2f51f7
zack <zack@squishymedia.com>
2016-09-30 14:50:29 -0700

    Separated out basic json validation for reusability

---
+++ utils/json.js
@@ -0,0 +1,61 @@
+var Issue  = require('./issues').Issue;
+var JSHINT = require('jshint').JSHINT;
+
+module.exports = {
+
+	/**
+	 * Parse
+	 *
+	 * Similar to native JSON.parse but uses
+	 * a callback structure, jshint for more
+	 * thorough error reporting and error formatting
+	 * of the rest of the validator.
+	 */
+	parse: function (file, contents, callback) {
+	    var jsObj = null;
+	    var err   = null;
+	    try {
+	        jsObj = JSON.parse(contents);
+	    }
+	    catch (exception) {
+	        err = exception;
+	    }
+	    finally {
+		    if (err) {
+		    	this.jshint(file, contents, (issues) => {
+		        	callback(issues, null);
+		        });
+		    } else {
+			    callback([], jsObj);
+		    }
+	    }
+	},
+
+	/**
+     * JSHint
+     *
+     * Checks known invalid JSON file
+     * content in order to produce a
+     * verbose error message.
+     */
+    jshint: function (file, contents, callback) {
+    	var issues = [];
+        if (!JSHINT(contents)) {
+            var out = JSHINT.data();
+            for (var i = 0; out.errors.length > i; ++i) {
+                var error = out.errors[i];
+                if (error) {
+                    issues.push(new Issue({
+                        code:      27,
+                        file:      file,
+                        line:      error.line      ? error.line      : null,
+                        character: error.character ? error.character : null,
+                        reason:    error.reason    ? error.reason    : null,
+                        evidence:  error.evidence  ? error.evidence  : null
+                    }));
+                }
+            }
+        }
+        callback(issues);
+    }
+}
--- validators/json.js
+++ validators/json.js
@@ -1,6 +1,5 @@
-var JSHINT = require('jshint').JSHINT;
-var Issue = require('../utils').issues.Issue;
-
+var utils = require('../utils');
+var Issue = utils.issues.Issue;
 
 /**
  * JSON
@@ -17,90 +16,60 @@ module.exports = function (file, contents, callback) {
     var issues = [];
     var jsObj  = null;
 
-    try {
-        jsObj = JSON.parse(contents);
-    }
-    catch (err) {
-        jshint(contents);
-    }
+    utils.json.parse(file, contents, function (pissues, jsObj) {
+        issues = pissues;
+        if (jsObj) {issues = issues.concat(checkUnits(file, jsObj));}
+        callback(issues, jsObj);
+    });
 
-    if (jsObj) {
-        checkUnits(jsObj);
-    }
-    callback(issues, jsObj);
+};
 
 // individual checks ---------------------------------------------------------------
 
-    /**
-     * JSHint
-     *
-     * Checks known invalid JSON file
-     * content in order to produce a
-     * verbose error message.
-     */
-    function jshint (contents) {
-        if (!JSHINT(contents)) {
-            var out = JSHINT.data();
-            for(var i = 0; out.errors.length > i; ++i){
-                var error = out.errors[i];
-                if(error){
-                    issues.push(new Issue({
-                        code:      27,
-                        file:      file,
-                        line:      error.line      ? error.line      : null,
-                        character: error.character ? error.character : null,
-                        reason:    error.reason    ? error.reason    : null,
-                        evidence:  error.evidence  ? error.evidence  : null
-                    }));
-                }
-            }
-        }
+function checkUnits (file, sidecar) {
+    var issues = [];
+    if (sidecar.hasOwnProperty('RepetitionTime') && sidecar["RepetitionTime"] > 100) {
+        issues.push(new Issue({
+            file: file,
+            code: 2
+        }));
     }
 
-    function checkUnits (sidecar) {
-        if (sidecar.hasOwnProperty('RepetitionTime') && sidecar["RepetitionTime"] > 100) {
-            issues.push(new Issue({
-                file: file,
-                code: 2
-            }));
-        }
-
-        if (sidecar.hasOwnProperty('EchoTime') && sidecar["EchoTime"] > 1) {
-            issues.push(new Issue({
-                file: file,
-                code: 3
-            }));
-        }
-        if (sidecar.hasOwnProperty('EchoTime1') && sidecar["EchoTime1"] > 1) {
-            issues.push(new Issue({
-                file: file,
-                code: 4
-            }));
-        }
-        if (sidecar.hasOwnProperty('EchoTime2') && sidecar["EchoTime2"] > 1) {
-            issues.push(new Issue({
-                file: file,
-                code: 4
-            }));
-        }
-        if (sidecar.hasOwnProperty('TotalReadoutTime') && sidecar["TotalReadoutTime"] > 10) {
-            issues.push(new Issue({
-                file: file,
-                code: 5
-            }));
-        }
-        if (sidecar.hasOwnProperty('PhaseEncodingDirection') && ["i", "i-", "j", "j-", "k", "k-"].indexOf(sidecar["PhaseEncodingDirection"]) == -1) {
-            issues.push(new Issue({
-                file: file,
-                code: 34
-            }));
-        }
-        if (sidecar.hasOwnProperty('SliceEncodingDirection') && ["i", "i-", "j", "j-", "k", "k-"].indexOf(sidecar["SliceEncodingDirection"]) == -1) {
-            issues.push(new Issue({
-                file: file,
-                code: 35
-            }));
-        }
+    if (sidecar.hasOwnProperty('EchoTime') && sidecar["EchoTime"] > 1) {
+        issues.push(new Issue({
+            file: file,
+            code: 3
+        }));
     }
-
-};
+    if (sidecar.hasOwnProperty('EchoTime1') && sidecar["EchoTime1"] > 1) {
+        issues.push(new Issue({
+            file: file,
+            code: 4
+        }));
+    }
+    if (sidecar.hasOwnProperty('EchoTime2') && sidecar["EchoTime2"] > 1) {
+        issues.push(new Issue({
+            file: file,
+            code: 4
+        }));
+    }
+    if (sidecar.hasOwnProperty('TotalReadoutTime') && sidecar["TotalReadoutTime"] > 10) {
+        issues.push(new Issue({
+            file: file,
+            code: 5
+        }));
+    }
+    if (sidecar.hasOwnProperty('PhaseEncodingDirection') && ["i", "i-", "j", "j-", "k", "k-"].indexOf(sidecar["PhaseEncodingDirection"]) == -1) {
+        issues.push(new Issue({
+            file: file,
+            code: 34
+        }));
+    }
+    if (sidecar.hasOwnProperty('SliceEncodingDirection') && ["i", "i-", "j", "j-", "k", "k-"].indexOf(sidecar["SliceEncodingDirection"]) == -1) {
+        issues.push(new Issue({
+            file: file,
+            code: 35
+        }));
+    }
+    return issues;
+}
