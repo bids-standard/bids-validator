c8cb0ead436f694658cea5b2c13b90d5a5d0aa2d
Admin <admin@psy-qp02708q5pm.stanford.edu>
2017-02-21 14:34:13 -0800

    validate _beh.json files

--- utils/type.js
+++ utils/type.js
@@ -39,8 +39,11 @@ module.exports = {
         var fixedTopLevelNames = ["/README", "/CHANGES", "/dataset_description.json", "/participants.tsv",
             "participants.json", "/phasediff.json", "/phase1.json", "/phase2.json" ,"/fieldmap.json"];
 
-        var funcTopRe = new RegExp('^\\/(?:ses-[a-zA-Z0-9]+_)?task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?'
-            + '(_bold.json|_sbref.json|_events.json|_events.tsv|_physio.json|_stim.json)$');
+        // var funcTopRe = new RegExp('^\\/(?:ses-[a-zA-Z0-9]+_)?task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?'
+        //     + '(_bold.json|_sbref.json|_events.json|_events.tsv|_physio.json|_stim.json)$');
+
+        var funcTopRe = new RegExp('^\\/(?:ses-[a-zA-Z0-9]+_)?(?:recording-[a-zA-Z0-9]+_)?task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+)?'
+            + '(_bold.json|_sbref.json|_events.json|_events.tsv|_physio.json|_stim.json|_beh.json)$');
 
         var anatTopRe = new RegExp('^\\/(?:ses-[a-zA-Z0-9]+_)?(?:_acq-[a-zA-Z0-9]+)?(?:_rec-[a-zA-Z0-9]+)?(?:_run-[0-9]+_)?'
             + '(' + anatSuffixes.join("|") + ').json$');
