388593566e1e30c7c46ef82bcb68a27a56b0efb5
Admin <admin@psy-qp02708q5pm.stanford.edu>
2017-02-22 12:53:10 -0800

    tests

--- tests/tsv.spec.js
+++ tests/tsv.spec.js
@@ -103,8 +103,10 @@ describe('TSV', function(){
     });
 
     it("should not allow particpants with age 89 and above in participants.tsv file", function () {
-        var tsv = 'participant_id\theader-two\t4eader-three\n' +
-                  'sub-01\tgroup-A\t89\n';
+      var tsv = 'participant_id\theader-two\t4eader-three\n' +
+                'sub-01\tvalue-two\t89';
+        // var tsv = 'participant_id\theader-two\t4eader-three\n' +
+        //           'sub-01\tgroup-A\t89\n';
         validate.TSV(participantsFile, tsv, [], function (issues) {
             assert(issues.length === 1 && issues[0].code === 56);
         });
