8591f92f52d24d8653278571badcdfb20f43fc7a
Nell Hardcastle <nell@squishymedia.com>
2017-09-08 11:20:16 -0700

    Add a failing test for Array.prototype issues.

--- tests/bids.spec.js
+++ tests/bids.spec.js
@@ -106,4 +106,25 @@ var suite = describe('BIDS example datasets ', function() {
             isdone();
         });
     });
+
+    // Catch some browser specific iteration issues with for .. in loops
+    describe('with enumerable array prototype methods', function() {
+        before(function () {
+            // Patch in a potentially iterable prototype
+            Array.prototype.broken = function () { /* custom extension */ }
+        });
+        after(function () {
+            // Unpatch the above
+            delete Array.prototype.broken;
+        });
+        it('validates dataset with illegal characters in task name', function (isdone) {
+
+            var options = {ignoreNiftiHeaders: false};
+            validate.BIDS("tests/data/valid_filenames", options, function (issues) {
+                var errors = issues.errors;
+                assert(errors[0].code === '58');
+                isdone();
+            });
+        });
+    });
 });
