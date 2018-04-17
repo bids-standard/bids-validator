a6b2e987460c9a1781b9d6463cec37934f6f0af6
Chris Holdgraf <choldgraf@berkeley.edu>
2018-04-16 19:59:52 -0700

    adding photo to anat

--- utils/type.js
+++ utils/type.js
@@ -6,7 +6,7 @@
  * BIDS specification requirements.
  */
 var anatSuffixes = ["T1w", "T2w", "T1map", "T2map", "T1rho", "FLAIR", "PD", "PDT2", "inplaneT1", "inplaneT2","angio",
-    "SWImagandphase", "T2star", "FLASH", "PDmap"];
+    "SWImagandphase", "T2star", "FLASH", "PDmap", "photo"];
 
 module.exports = {
 
@@ -55,7 +55,7 @@ module.exports = {
         var megTopRe = new RegExp('^\\/(?:ses-[a-zA-Z0-9]+_)?task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_proc-[a-zA-Z0-9]+)?'
             + '(_meg.json|_channels.tsv|_photo.jpg|_coordsystem.json)$');
 
-        var ieegTopRe = new RegExp('^\\/(?:ses-[a-zA-Z0-9]+_)?task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_proc-[a-zA-Z0-9]+)?'
+        var ieegTopRe = new RegExp('^\\/(?:ses-[a-zA-Z0-9]+_)?task-[a-zA-Z0-9]+(?:_acq-[a-zA-Z0-9]+)?(?:_proc-[a-zA-Z0-9]+)?(?:_space-[a-zA-Z0-9]+)?'
             + '(_ieeg.json|_channels.tsv|_electrodes.tsv|_photo.jpg|_coordsystem.json)$');
 
         var otherTopFiles = new RegExp('^\\/(?:ses-[a-zA-Z0-9]+_)?(?:recording-[a-zA-Z0-9]+_)?(?:task-[a-zA-Z0-9]+_)?(?:acq-[a-zA-Z0-9]+_)?(?:rec-[a-zA-Z0-9]+_)?(?:run-[0-9]+_)?'
