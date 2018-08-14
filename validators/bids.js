1a4436fc622950ae2111c5465f6a16d411d4b86b
Nell Hardcastle <nell@squishymedia.com>
2018-08-14 12:26:31 -0700

    Merge EEG/iEEG for dataExtRE due to overlap.

--- validators/bids.js
+++ validators/bids.js
@@ -246,8 +246,7 @@ BIDS = {
             var dataExtRE = new RegExp (['^.*\\.(',
                                          'nii|nii\\.gz|', // MRI
                                          'fif|fif\\.gz|sqd|con|kdf|chn|trg|raw|raw\\.mhf|', // MEG
-                                         'eeg|vhdr|vmrk|edf|cnt|bdf|set|fdt', // EEG
-                                         'dat|nwb|tdat|tidx|tmet', // iEEG
+                                         'eeg|vhdr|vmrk|edf|cnt|bdf|set|fdt|dat|nwb|tdat|tidx|tmet', // EEG/iEEG
                                          ')$'].join(''));
 
             // ignore associated data
