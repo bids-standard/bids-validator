359245ec442a7250ed94e16a01d02e1cdfdaed42
Teal Hobson-Lowther <teal@squishymedia.com>
2018-09-12 13:22:09 -0700

    _magnitude1.nii[.gz] and _magnitude2.nii[.gz] should have exactly 3 dimensions

--- tests/headerField.spec.js
+++ tests/headerField.spec.js
@@ -2,10 +2,9 @@ const assert = require('assert')
 const headerFields = require('../validators/headerFields')
 
 describe('headerFields', () => {
-  it('should throw an error if _magnitude1 or _magnitude2 files have too many dimensions.', () => {
-    // each of these headers has one too many dimensions on the 'dim' field.
-    // the first entry is the total count, and the following three entries are spatial.
+  it('should throw an error if _magnitude1 or _magnitude2 files do not have exactly dimensions.', () => {
     const headers = [
+      // each of these headers has one too many dimensions on the 'dim' field.
       [
         {
           name: 'sub-01_magnitude1.nii',
@@ -28,10 +27,37 @@ describe('headerFields', () => {
           xyzt_units: [5, 1, 1, 1, 1],
         },
       ],
+      // each of these headers has one too few dimensions on the 'dim' field.
+      [
+        {
+          name: 'sub-02_magnitude1.nii',
+          relativePath: 'sub-02_magnitude1.nii',
+        },
+        {
+          dim: [3, 1, 1],
+          pixdim: [4, 1, 1, 1],
+          xyzt_units: [4, 1, 1, 1],
+        },
+      ],
+      [
+        {
+          name: 'sub-02_magnitude2.nii',
+          relativePath: 'sub-02_magnitude2.nii',
+        },
+        {
+          dim: [3, 1, 1],
+          pixdim: [4, 1, 1, 1],
+          xyzt_units: [4, 1, 1, 1],
+        },
+      ],
     ]
     const issues = headerFields(headers)
     assert(
-      issues.length == 2 && issues[0].code == '94' && issues[1].code == '94',
+      issues.length == 4 &&
+        issues[0].code == '94' &&
+        issues[1].code == '94' &&
+        issues[2].code == '94' &&
+        issues[3].code == '94',
     )
   })
 
@@ -54,7 +80,7 @@ describe('headerFields', () => {
           relativePath: 'sub-01_magnitude2.nii',
         },
         {
-          dim: [3, 1, 1],
+          dim: [4, 1, 1, 1],
           pixdim: [4, 1, 1, 1],
           xyzt_units: [4, 1, 1, 1],
         },
