f76a778250424abdfae18c830f3a07b5ae37a17f
Teal Hobson-Lowther <tealhobsonlowther@gmail.com>
2018-09-19 11:18:37 -0700

    create summary primitive in bids/obj.js

--- validators/bids/obj.js
+++ validators/bids/obj.js
@@ -1,4 +1,12 @@
 module.exports = {
   options: {},
   issues: [],
+  summary: {
+    sessions: [],
+    subjects: [],
+    tasks: [],
+    modalities: [],
+    totalFiles: [],
+    size: 0,
+  },
 }
