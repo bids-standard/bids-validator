50dd3cb1063b26e1553ac8305d183ac2b6aee665
Teal Hobson-Lowther <tealhobsonlowther@gmail.com>
2018-09-20 14:36:45 -0700

    bval and bvec module defaults refined

--- validators/index.js
+++ validators/index.js
@@ -1,19 +1,19 @@
 // dependencies ------------------------------------------------------
 
 var tsv = require('./tsv')
-var JSON = require('./json')
+var JSON = require('./json').json
 var NIFTI = require('./nii')
 var BIDS = require('./bids').start
 var Events = require('./events')
-var bval = require('./bval')
-var bvec = require('./bvec')
+var bval = require('./bval').bval
+var bvec = require('./bvec').bvec
 var utils = require('../utils')
 
 // public api --------------------------------------------------------
 
 var validate = {
   BIDS: BIDS,
-  JSON: JSON.json,
+  JSON: JSON,
   TSV: tsv,
   NIFTI: NIFTI,
   Events: Events,
