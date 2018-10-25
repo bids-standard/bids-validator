3d0b52dc7f70965d0d1868728a60f680025e0578
David Nishikawa <nishikawa.david@gmail.com>
2018-10-25 10:41:00 -0700

    promise refactor to flatten

--- validators/bids/fullTest.js
+++ validators/bids/fullTest.js
@@ -92,105 +92,95 @@ const fullTest = (fileList, options, callback) => {
       self.issues = self.issues.concat(tsvIssues)
 
       // Bvec validation
-      bvec.validate(files.bvec, bContentsDict).then(bvecIssues => {
-        self.issues = self.issues.concat(bvecIssues)
+      return bvec.validate(files.bvec, bContentsDict)
+    })
+    .then(bvecIssues => {
+      self.issues = self.issues.concat(bvecIssues)
 
-        // Bval validation
-        bval.validate(files.bval, bContentsDict).then(bvalIssues => {
-          self.issues = self.issues.concat(bvalIssues)
+      // Bval validation
+      return bval.validate(files.bval, bContentsDict)
+    })
+    .then(bvalIssues => {
+      self.issues = self.issues.concat(bvalIssues)
 
-          // Load json files and construct a contents object with field, value pairs
-          json
-            .load(files.json, jsonFiles, jsonContentsDict)
-            .then(jsonLoadIssues => {
-              self.issues = self.issues.concat(jsonLoadIssues)
+      // Load json files and construct a contents object with field, value pairs
+      return json.load(files.json, jsonFiles, jsonContentsDict)
+    })
+    .then(jsonLoadIssues => {
+      self.issues = self.issues.concat(jsonLoadIssues)
 
-              // Check for at least one subject
-              const noSubjectIssues = subjects.atLeastOneSubject(fileList)
-              self.issues = self.issues.concat(noSubjectIssues)
+      // Check for at least one subject
+      const noSubjectIssues = subjects.atLeastOneSubject(fileList)
+      self.issues = self.issues.concat(noSubjectIssues)
 
-              // Check for datasetDescription file in the proper place
-              const datasetDescriptionIssues = checkDatasetDescription(fileList)
-              self.issues = self.issues.concat(datasetDescriptionIssues)
+      // Check for datasetDescription file in the proper place
+      const datasetDescriptionIssues = checkDatasetDescription(fileList)
+      self.issues = self.issues.concat(datasetDescriptionIssues)
 
-              // Validate json files and contents
-              json
-                .validate(jsonFiles, fileList, jsonContentsDict, summary)
-                .then(jsonIssues => {
-                  self.issues = self.issues.concat(jsonIssues)
+      // Validate json files and contents
+      return json.validate(jsonFiles, fileList, jsonContentsDict, summary)
+    })
+    .then(jsonIssues => {
+      self.issues = self.issues.concat(jsonIssues)
 
-                  // Nifti validation
-                  NIFTI.validate(
-                    files.nifti,
-                    fileList,
-                    self.options,
-                    jsonContentsDict,
-                    bContentsDict,
-                    events,
-                    headers,
-                  ).then(niftiIssues => {
-                    self.issues = self.issues.concat(niftiIssues)
+      // Nifti validation
+      return NIFTI.validate(
+        files.nifti,
+        fileList,
+        self.options,
+        jsonContentsDict,
+        bContentsDict,
+        events,
+        headers,
+      )
+    })
+    .then(niftiIssues => {
+      self.issues = self.issues.concat(niftiIssues)
 
-                    // Issues related to participants not listed in the subjects list
-                    const participantsInSubjectsIssues = subjects.participantsInSubjects(
-                      participants,
-                      summary.subjects,
-                    )
-                    self.issues = self.issues.concat(
-                      participantsInSubjectsIssues,
-                    )
+      // Issues related to participants not listed in the subjects list
+      const participantsInSubjectsIssues = subjects.participantsInSubjects(
+        participants,
+        summary.subjects,
+      )
+      self.issues = self.issues.concat(participantsInSubjectsIssues)
 
-                    // Check for equal number of participants from ./phenotype/*.tsv and participants in dataset
-                    const phenotypeIssues = tsv.checkPhenotype(
-                      phenotypeParticipants,
-                      summary,
-                    )
-                    self.issues = self.issues.concat(phenotypeIssues)
+      // Check for equal number of participants from ./phenotype/*.tsv and participants in dataset
+      const phenotypeIssues = tsv.checkPhenotype(phenotypeParticipants, summary)
+      self.issues = self.issues.concat(phenotypeIssues)
 
-                    // Validate nii header fields
-                    self.issues = self.issues.concat(headerFields(headers))
+      // Validate nii header fields
+      self.issues = self.issues.concat(headerFields(headers))
 
-                    // Events validation
-                    stimuli.directory = files.stimuli
-                    const eventsIssues = Events.validateEvents(
-                      events,
-                      stimuli,
-                      headers,
-                      jsonContentsDict,
-                      self.issues,
-                    )
-                    self.issues = self.issues.concat(eventsIssues)
+      // Events validation
+      stimuli.directory = files.stimuli
+      const eventsIssues = Events.validateEvents(
+        events,
+        stimuli,
+        headers,
+        jsonContentsDict,
+        self.issues,
+      )
+      self.issues = self.issues.concat(eventsIssues)
 
-                    // Validate custom fields in all TSVs and add any issues to the list
-                    self.issues = self.issues.concat(
-                      tsv.validateTsvColumns(tsvs, jsonContentsDict),
-                    )
+      // Validate custom fields in all TSVs and add any issues to the list
+      self.issues = self.issues.concat(
+        tsv.validateTsvColumns(tsvs, jsonContentsDict),
+      )
 
-                    // Validate session files
-                    self.issues = self.issues.concat(session(fileList))
+      // Validate session files
+      self.issues = self.issues.concat(session(fileList))
 
-                    // Determine if each subject has data present
-                    self.issues = self.issues.concat(
-                      checkAnyDataPresent(fileList, summary.subjects),
-                    )
+      // Determine if each subject has data present
+      self.issues = self.issues.concat(
+        checkAnyDataPresent(fileList, summary.subjects),
+      )
 
-                    // Group summary modalities
-                    summary.modalities = utils.modalities.group(
-                      summary.modalities,
-                    )
+      // Group summary modalities
+      summary.modalities = utils.modalities.group(summary.modalities)
 
-                    // Format issues
-                    const issues = utils.issues.format(
-                      self.issues,
-                      summary,
-                      self.options,
-                    )
-                    callback(issues, summary)
-                  })
-                })
-            })
-        })
-      })
+      // Format issues
+      const issues = utils.issues.format(self.issues, summary, self.options)
+      callback(issues, summary)
     })
 }
 
