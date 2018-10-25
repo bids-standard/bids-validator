2db37d3a7b1bacb762ccf786c843b5368d36b053
David Nishikawa <nishikawa.david@gmail.com>
2018-10-25 10:40:30 -0700

    remove unecessary catch and eslint--fix

--- validators/tsv/validate.js
+++ validators/tsv/validate.js
@@ -14,51 +14,45 @@ const validate = (
   // validate tsv
   const tsvPromises = files.map(function(file) {
     return new Promise(resolve => {
-      utils.files
-        .readFile(file)
-        .then(contents => {
-          // Push TSV to list for custom column verification after all data dictionaries have been read
-          tsvs.push({
+      utils.files.readFile(file).then(contents => {
+        // Push TSV to list for custom column verification after all data dictionaries have been read
+        tsvs.push({
+          file: file,
+          contents: contents,
+        })
+        if (file.name.endsWith('_events.tsv')) {
+          events.push({
             file: file,
+            path: file.relativePath,
             contents: contents,
           })
-          if (file.name.endsWith('_events.tsv')) {
-            events.push({
-              file: file,
-              path: file.relativePath,
-              contents: contents,
-            })
-          }
-          tsv(file, contents, fileList, function(
-            tsvIssues,
-            participantList,
-            stimFiles,
-          ) {
-            if (participantList) {
-              if (file.name.endsWith('participants.tsv')) {
-                participants = {
-                  list: participantList,
-                  file: file,
-                }
-              } else if (file.relativePath.includes('phenotype/')) {
-                phenotypeParticipants.push({
-                  list: participantList,
-                  file: file,
-                })
+        }
+        tsv(file, contents, fileList, function(
+          tsvIssues,
+          participantList,
+          stimFiles,
+        ) {
+          if (participantList) {
+            if (file.name.endsWith('participants.tsv')) {
+              participants = {
+                list: participantList,
+                file: file,
               }
+            } else if (file.relativePath.includes('phenotype/')) {
+              phenotypeParticipants.push({
+                list: participantList,
+                file: file,
+              })
             }
-            if (stimFiles && stimFiles.length) {
-              // add unique new events to the stimuli.events array
-              stimuli.events = [...new Set([...stimuli.events, ...stimFiles])]
-            }
-            issues = issues.concat(tsvIssues)
-            return resolve()
-          })
-        })
-        .catch(issue => {
-          issues = issues.concat(issue)
+          }
+          if (stimFiles && stimFiles.length) {
+            // add unique new events to the stimuli.events array
+            stimuli.events = [...new Set([...stimuli.events, ...stimFiles])]
+          }
+          issues = issues.concat(tsvIssues)
           return resolve()
         })
+      })
     })
   })
 
