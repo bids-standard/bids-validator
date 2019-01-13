6e069e31fef8ea445ca3a363db9ae669621b3c47
David Nishikawa <david@squishymedia.com>
2019-01-12 23:18:55 -0800

    refactor large function

--- validators/json/load.js
+++ validators/json/load.js
@@ -1,5 +1,20 @@
 const utils = require('../../utils')
 
+function validate(resolve, reject, file, jsonFiles, jsonContentsDict, issues) {
+  return function(parseIssues, jsObj) {
+    issues = issues.concat(parseIssues)
+
+    // abort further tests if schema test does not pass
+    if (issues.some(issue => issue.severity === 'error')) {
+      return reject()
+    }
+
+    jsonContentsDict[file.relativePath] = jsObj
+    jsonFiles.push(file)
+    resolve()
+  }
+}
+
 const load = (files, jsonFiles, jsonContentsDict, annexed, dir) => {
   let issues = []
   const jsonPromises = files.map(function(file) {
@@ -9,18 +24,18 @@ const load = (files, jsonFiles, jsonContentsDict, annexed, dir) => {
           utils.files
             .readFile(file, annexed, dir)
             .then(contents => {
-              utils.json.parse(file, contents, function(parseIssues, jsObj) {
-                issues = issues.concat(parseIssues)
-
-                // abort further tests if schema test does not pass
-                if (issues.some(issue => issue.severity === 'error')) {
-                  return reject()
-                }
-
-                jsonContentsDict[file.relativePath] = jsObj
-                jsonFiles.push(file)
-                resolve()
-              })
+              utils.json.parse(
+                file,
+                contents,
+                validate(
+                  resolve,
+                  reject,
+                  file,
+                  jsonFiles,
+                  jsonContentsDict,
+                  issues,
+                ),
+              )
             })
             .catch(issue => {
               issues.push(issue)
