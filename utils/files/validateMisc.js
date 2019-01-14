388bdb4a57820a5974584efd41b126195ca4dd66
David Nishikawa <david@squishymedia.com>
2019-01-14 15:11:11 -0800

    take into account filesize data location in browser vs node

--- utils/files/validateMisc.js
+++ utils/files/validateMisc.js
@@ -1,7 +1,8 @@
 const Issue = require('../issues/issue')
 
 function createIssueForEmpty(file) {
-  return file.stats.size <= 0 && new Issue({ code: 99, file: file })
+  const size = typeof window !== 'undefined' ? file.size : file.stats.size
+  return size <= 0 && new Issue({ code: 99, file: file })
 }
 function clearNonIssues(x) {
   return x instanceof Issue
