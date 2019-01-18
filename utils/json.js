bc746143a0c2b0e9ddf65870e8635075e67e9b78
Nell Hardcastle <nell@squishymedia.com>
2019-01-17 16:25:30 -0800

    Refactor JSON load for readability.

--- utils/json.js
+++ utils/json.js
@@ -1,60 +1,61 @@
-var Issue = require('./issues').Issue
-var JSHINT = require('jshint').JSHINT
+const Issue = require('./issues').Issue
+const { JSHINT } = require('jshint')
 
-module.exports = {
-  /**
-   * Parse
-   *
-   * Similar to native JSON.parse but uses
-   * a callback structure, jshint for more
-   * thorough error reporting and error formatting
-   * of the rest of the validator.
-   */
-  parse: function(file, contents, callback) {
-    var jsObj = null
-    var err = null
+/**
+ * Similar to native JSON.parse but returns a promise and
+ * runs jshint for more thorough error reporting
+ */
+function parse(file, contents) {
+  return new Promise(resolve => {
+    let jsObj
+    let err
     try {
       jsObj = JSON.parse(contents)
     } catch (exception) {
       err = exception
     } finally {
       if (err) {
-        this.jshint(file, contents, function(issues) {
-          callback(issues, null)
+        jshint(file, contents, function(issues) {
+          resolve({ issues, parsed: null })
         })
       } else {
-        callback([], jsObj)
+        resolve({ issues: [], parsed: jsObj })
       }
     }
-  },
+  })
+}
 
-  /**
-   * JSHint
-   *
-   * Checks known invalid JSON file
-   * content in order to produce a
-   * verbose error message.
-   */
-  jshint: function(file, contents, callback) {
-    var issues = []
-    if (!JSHINT(contents)) {
-      var out = JSHINT.data()
-      for (var i = 0; out.errors.length > i; ++i) {
-        var error = out.errors[i]
-        if (error) {
-          issues.push(
-            new Issue({
-              code: 27,
-              file: file,
-              line: error.line ? error.line : null,
-              character: error.character ? error.character : null,
-              reason: error.reason ? error.reason : null,
-              evidence: error.evidence ? error.evidence : null,
-            }),
-          )
-        }
+/**
+ * JSHint
+ *
+ * Checks known invalid JSON file
+ * content in order to produce a
+ * verbose error message.
+ */
+function jshint(file, contents, callback) {
+  var issues = []
+  if (!JSHINT(contents)) {
+    var out = JSHINT.data()
+    for (var i = 0; out.errors.length > i; ++i) {
+      var error = out.errors[i]
+      if (error) {
+        issues.push(
+          new Issue({
+            code: 27,
+            file: file,
+            line: error.line ? error.line : null,
+            character: error.character ? error.character : null,
+            reason: error.reason ? error.reason : null,
+            evidence: error.evidence ? error.evidence : null,
+          }),
+        )
       }
     }
-    callback(issues)
-  },
+  }
+  callback(issues)
+}
+
+module.exports = {
+  parse,
+  jshint,
 }
--- validators/json/load.js
+++ validators/json/load.js
@@ -1,61 +1,34 @@
 const utils = require('../../utils')
 
-function validate({
-  resolve,
-  reject,
-  file,
-  jsonFiles,
-  jsonContentsDict,
-  issues,
-}) {
-  return function(parseIssues, jsObj) {
-    issues = issues.concat(parseIssues)
-
-    // abort further tests if schema test does not pass
-    if (issues.some(issue => issue.severity === 'error')) {
-      return reject()
-    }
-
-    jsonContentsDict[file.relativePath] = jsObj
-    jsonFiles.push(file)
-    resolve()
-  }
-}
-
 const load = (files, jsonFiles, jsonContentsDict, annexed, dir) => {
   let issues = []
-  const jsonPromises = files.map(function(file) {
-    return utils.limit(
-      () =>
-        new Promise((resolve, reject) => {
-          utils.files
-            .readFile(file, annexed, dir)
-            .then(contents => {
-              utils.json.parse(
-                file,
-                contents,
-                validate({
-                  resolve,
-                  reject,
-                  file,
-                  jsonFiles,
-                  jsonContentsDict,
-                  issues,
-                }),
-              )
-            })
-            .catch(issue => {
-              issues.push(issue)
-              resolve()
-            })
-        }),
-    )
-  })
-  return new Promise(resolve =>
-    Promise.all(jsonPromises)
-      .then(() => resolve(issues))
-      .catch(() => resolve(issues)),
+
+  // Read JSON file contents and parse for issues
+  const readJsonFile = (file, annexed, dir) =>
+    utils.files
+      .readFile(file, annexed, dir)
+      .then(contents => utils.json.parse(file, contents))
+      .then(({ issues: parseIssues, parsed }) => {
+        // Append any parse issues to returned issues
+        Array.prototype.push.apply(issues, parseIssues)
+
+        // Abort further tests if an error is found
+        if (
+          parseIssues &&
+          !parseIssues.some(issue => issue.severity === 'error')
+        ) {
+          jsonContentsDict[file.relativePath] = parsed
+          jsonFiles.push(file)
+        }
+      })
+
+  // Start concurrent read/parses
+  const fileReads = files.map(file =>
+    utils.limit(() => readJsonFile(file, annexed, dir)),
   )
+
+  // After all reads/parses complete, return any found issues
+  return Promise.all(fileReads).then(() => issues)
 }
 
 module.exports = load
