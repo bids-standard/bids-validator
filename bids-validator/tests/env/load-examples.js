ef0ed9db12d68a539f899fab3bd4a32206280a1c
Nell Hardcastle <nell@squishymedia.com>
2019-03-15 15:36:41 -0700

    Fix build environment locking conflict.

--- bids-validator/tests/env/load-examples.js
+++ bids-validator/tests/env/load-examples.js
@@ -1,10 +1,19 @@
 const fs = require('fs')
+const { promisify } = require('util')
 const request = require('sync-request')
 const AdmZip = require('adm-zip')
+const lockfile = require('lockfile')
+
+const lockPromise = promisify(lockfile.lock)
 
 const test_version = '1.1.1u1'
+const examples_lock = 'bids-validator/tests/data/examples.lockfile'
+// Wait for up to five minutes for examples to finish
+// downloading in another test worker
+const examples_lock_opts = { wait: 300000 }
 
-const loadExamples = () => {
+const loadExamples = async () => {
+  await lockPromise(examples_lock, examples_lock_opts)
   if (!fs.existsSync('tests/data/bids-examples-' + test_version + '/')) {
     console.log('downloading test data')
     const response = request(
@@ -21,6 +30,7 @@ const loadExamples = () => {
     console.log('unzipping test data')
     zip.extractAllTo('bids-validator/tests/data/', true)
   }
+  lockfile.unlockSync(examples_lock)
   return test_version
 }
 
