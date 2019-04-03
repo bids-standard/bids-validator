28ad75247757120215016b3a27fc1ef1b2ce3da9
Nell Hardcastle <nell@squishymedia.com>
2019-04-03 15:32:52 -0700

    Add a test for the issue in #737

---
+++ bids-validator/utils/__tests__/type.spec.js
@@ -0,0 +1,13 @@
+const type = require('../type.js')
+
+describe('type.js', () => {
+  describe('isBids()', () => {
+    it('does not throw an error for valid defacemask filenames', () => {
+      expect(
+        type.isBIDS(
+          '/sub-rid000043/anat/sub-rid000043_run-02_mod-T1w_defacemask.nii.gz',
+        ),
+      ).toBe(true)
+    })
+  })
+})
