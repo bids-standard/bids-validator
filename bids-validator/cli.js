275513b4b842f303566a4b8410c72e6ad28edda5
David Nishikawa <david@squishymedia.com>
2019-05-07 16:49:51 -0700

    exit process (w/ --json arg) code 1 if errors

--- bids-validator/cli.js
+++ bids-validator/cli.js
@@ -5,24 +5,30 @@ var colors = require('colors/safe')
 var fs = require('fs')
 const remoteFiles = require('./utils/files/remoteFiles')
 
+const exitProcess = issues => {
+  if (
+    issues === 'Invalid' ||
+    (issues.errors && issues.errors.length >= 1) ||
+    (issues.config && issues.config.length >= 1)
+  ) {
+    process.exit(1)
+  } else {
+    process.exit(0)
+  }
+}
+
 module.exports = function(dir, options) {
   if (fs.existsSync(dir)) {
     if (options.json) {
       validate.BIDS(dir, options, function(issues, summary) {
         console.log(JSON.stringify({ issues, summary }))
+        exitProcess(issues)
       })
     } else {
       validate.BIDS(dir, options, function(issues, summary) {
         console.log(validate.consoleFormat.issues(issues, options) + '\n')
         console.log(validate.consoleFormat.summary(summary, options))
-
-        if (
-          issues === 'Invalid' ||
-          (issues.errors && issues.errors.length >= 1) ||
-          (issues.config && issues.config.length >= 1)
-        ) {
-          process.exit(1)
-        }
+        exitProcess(issues)
       })
     }
   } else {
