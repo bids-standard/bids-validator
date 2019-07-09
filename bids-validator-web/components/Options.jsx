5f8472a16c06871e97294ffad7de18b2b9540919
David Nishikawa <david@squishymedia.com>
2019-07-09 09:08:38 -0700

    change options to checkboxes

--- bids-validator-web/components/Options.jsx
+++ bids-validator-web/components/Options.jsx
@@ -4,9 +4,9 @@ const Options = ({ setOption, options }) => (
   <>
     <form className="options" onClick={setOption}>
       <label><strong>Options:</strong></label>
-      <input name="ignoreWarnings" type="radio" checked={options.ignoreWarnings} readOnly />
+      <input name="ignoreWarnings" type="checkbox" checked={options.ignoreWarnings} readOnly />
       <label htmlFor="ignoreWarnings">Ignore Warnings</label>
-      <input name="ignoreNiftiHeaders" type="radio" checked={options.ignoreNiftiHeaders} readOnly />
+      <input name="ignoreNiftiHeaders" type="checkbox" checked={options.ignoreNiftiHeaders} readOnly />
       <label htmlFor="ignoreNiftiHeaders">Ignore NIfTI Headers</label>
     </form>
     <hr />
