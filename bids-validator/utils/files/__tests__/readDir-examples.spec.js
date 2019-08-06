97cf03bcca2707675589ee5de86b3b0c0a54a959
Nell Hardcastle <nell@squishymedia.com>
2019-08-06 10:36:45 -0700

    Rename existing readDir test to readDir-examples.
    readDir should be the unit tests, readDir-examples are integration.

---
+++ bids-validator/utils/files/__tests__/readDir-examples.spec.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment ./bids-validator/tests/env/ExamplesEnvironment.js
+ */
+const readDir = require('../readDir.js').default
+
+describe('readDir.js', () => {
+  describe('getFiles()', () => {
+    it('returns expected files', async done => {
+      readDir('bids-validator/tests/data/bids-examples-1.2.0/ds002/').then(
+        files => {
+          expect(Object.keys(files)).toHaveLength(245)
+          expect(files[0].name).toBe('CHANGES')
+          expect(files[25].name).toBe(
+            'sub-02_task-mixedeventrelatedprobe_run-01_events.tsv',
+          )
+          expect(files[200].name).toBe('sub-15_T1w.nii.gz')
+          done()
+        },
+      )
+    })
+  })
+})
