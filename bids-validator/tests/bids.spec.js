4e9228112d4dc36c7a5d7e13383bd8b03cf48e65
David Nishikawa <david@squishymedia.com>
2019-09-09 14:53:49 -0700

    fix eslint error

--- bids-validator/tests/bids.spec.js
+++ bids-validator/tests/bids.spec.js
@@ -115,7 +115,12 @@ describe('BIDS example datasets ', function() {
       var warnings = issues.warnings
       assert(summary.sessions.length === 0)
       assert(summary.subjects.length === 1)
-      assert.deepEqual(summary.subjectMetadata, { '01': { sex: 'M', age: 25 } })
+      assert.deepEqual(summary.subjectMetadata, {
+        '01': {
+          sex: 'M',
+          age: 25,
+        },
+      })
       assert.deepEqual(summary.tasks, ['rhyme judgment'])
       assert.isFalse(summary.dataProcessed)
       assert(summary.modalities.includes('T1w'))
