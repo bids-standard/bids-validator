2f7517b09a8c284d2ed7ecf1c79b565a83608e31
David Nishikawa <david@squishymedia.com>
2019-11-18 15:04:56 -0800

    update submodule path in config

--- bids-validator/tests/env/config.js
+++ bids-validator/tests/env/config.js
@@ -3,5 +3,5 @@ const TEST_VERSION = '1.2.0'
 module.exports = {
   TEST_VERSION,
   GITHUB_URL: `https://github.com/bids-standard/bids-examples.git`,
-  SUBMODULE_PATH: `bids-validator/tests/data/bids-examples-${TEST_VERSION}/`,
+  SUBMODULE_PATH: `bids-validator/tests/data/bids-examples-${TEST_VERSION}`,
 }
