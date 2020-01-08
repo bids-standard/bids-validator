9a5c7e71e708dcf5c162eaa53f45165e0d4e508b
MatthewZito <matthewtzito@gmail.com>
2020-01-08 11:48:28 -0800

    remove settimeouts in test suite

--- bids-validator/tests/cli.spec.js
+++ bids-validator/tests/cli.spec.js
@@ -34,7 +34,6 @@ describe('CLI', () => {
   })
 
   it('should accept a directory as the first argument without error', done => {
-    jest.setTimeout(30000)
     const command = spawn('node', [cli_path, test_data])
     let commandOutput = []
     command.stderr.on('data', data => {
@@ -48,7 +47,6 @@ describe('CLI', () => {
   })
 
   it('should accept an array of options as the second argument without error', done => {
-    jest.setTimeout(30000)
     const command = spawn('node', [cli_path, test_data, '--json'])
     let commandOutput = []
     command.stderr.on('data', data => {
@@ -78,7 +76,6 @@ describe('CLI', () => {
   })
 
   it('with errors should not exit with code 0 with --json argument', done => {
-    jest.setTimeout(30000)
     const command = spawn('node', [cli_path, data_with_errors, '--json'])
     let commandOutput = []
     let output = {}
