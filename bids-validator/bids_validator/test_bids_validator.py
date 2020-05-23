f715b898fb2c6cc9d2f4248e1f2c6b95bcea97cd
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2020-05-23 19:14:46 +0200

    fix python tests

--- bids-validator/bids_validator/test_bids_validator.py
+++ bids-validator/bids_validator/test_bids_validator.py
@@ -5,46 +5,48 @@ actual file contents.
 
 """
 import os
+
+import pytest
 import datalad.api
 
 from bids_validator import BIDSValidator
 
+HOME = os.path.expanduser('~')
 
-def test_is_bids():
-    """Test that is_bids returns true for each file in a valid BIDS dataset."""
-    # Download testing data as git annex dataset
-    HOME = os.path.expanduser('~')
-    dspath = os.path.join(HOME, 'eeg_matchingpennies_git_annex')
-    url = 'https://github.com/sappelhoff/eeg_matchingpennies'
+TEST_DATA_DICT = {
+    'eeg_matchingpennies': 'https://github.com/sappelhoff/eeg_matchingpennies'
+    }
+
+EXCLUDE_KEYWORDS = ['git', 'datalad', 'sourcedata', 'bidsignore', 'LICENSE']
+
+
+def _download_test_data(test_data_dict, dsname):
+    """Download test data using datalad."""
+    url = test_data_dict[dsname]
+    dspath = os.path.join(HOME, dsname)
     datalad.api.install(dspath, url)
+    return dspath
 
-    def _exclude_this(fname):
-        """Help to skip certain files from being validated."""
-        exclude_keywords = ['git', 'datalad', 'sourcedata', 'bidsignore']
-        for keyword in exclude_keywords:
-            if keyword in fname:
-                return True
 
-        return False
-
-    # Gather all files
+def _gather_test_files(dspath, exclude_keywords):
+    """Get test files from dataset path, relative to dataset."""
     files = []
     for r, _, f in os.walk(dspath):
         for file in f:
             fname = os.path.join(r, file)
-            if not _exclude_this(fname):
+            fname = fname.replace(dspath, '')
+            if not any(keyword in fname for keyword in exclude_keywords):
                 files.append(fname)
 
-    # Test all files
+    return files
+
+
+dspath = _download_test_data(TEST_DATA_DICT, 'eeg_matchingpennies')
+files = _gather_test_files(dspath, EXCLUDE_KEYWORDS)
+
+
+@pytest.mark.parametrize('fname', files)
+def test_is_bids(fname):
+    """Test that is_bids returns true for each file in a valid BIDS dataset."""
     validator = BIDSValidator()
-    false_decisions = []
-    for file in files:
-        if not validator.is_bids(file):
-            false_decisions.append(file)
-
-    if len(false_decisions) > 0:
-        raise AssertionError('False decisions for the following files: {}'
-                             .format(false_decisions))
-
-    # Sanity check: Does it raise for wrong files?
-    assert not validator.is_bids('bogus')
+    assert validator.is_bids(fname)
