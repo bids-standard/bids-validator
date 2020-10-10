4e2b9758850a1052deb0c0bc108ce3139f9eb5ea
Ross Blair <rosswilsonblair@gmail.com>
2020-10-09 17:56:51 -0700

    add flag to skip session checks

--- bids-validator/utils/options.js
+++ bids-validator/utils/options.js
@@ -15,6 +15,7 @@ export default {
       ignoreWarnings: Boolean(options.ignoreWarnings),
       ignoreNiftiHeaders: Boolean(options.ignoreNiftiHeaders),
       ignoreSymlinks: Boolean(options.ignoreSymlinks),
+      ignoreSessionConsistency: Boolean(options.ignoreSessionConsistency),
       verbose: Boolean(options.verbose),
       gitTreeMode: Boolean(options.gitTreeMode),
       remoteFiles: Boolean(options.remoteFiles),
