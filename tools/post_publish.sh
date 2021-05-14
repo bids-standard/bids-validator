fddfaf363a39b595099e7087698ddf263f213b02
Nell Hardcastle <nell@squishymedia.com>
2021-05-14 06:43:49 -0700

    fix: Use a lifecycle script to create dev versions automatically after publish

--- tools/post_publish.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# Generate a dev version with no tag and push. Meant to be called immediately after publishing new version with tag/npm package.
-set -e
-./node_modules/.bin/lerna version prepatch --preid dev --no-git-tag-version
-git add bids-validator-web/package-lock.json bids-validator-web/package.json bids-validator/package-lock.json bids-validator/package.json lerna.json
-git commit -m "setting dev version"
-git push
