f24c13676cca4f73733eb9d2e01f49d3be0ebb15
Nell Hardcastle <nell@squishymedia.com>
2021-09-10 16:19:25 -0700

    feat: Use bids-specification schema for some validation rules
    Allows for the use of either a local copy or directly reading
    tagged versions from the bids-specification GitHub

--- bids-validator/utils/__tests__/type.spec.js
+++ bids-validator/utils/__tests__/type.spec.js
@@ -1,4 +1,5 @@
-import type from '../type.js'
+import { schemaRegex } from '../../src/schemaTypes.js'
+import type, { schemaSetup } from '../type.js'
 
 describe('type.js', () => {
   describe('isBids()', () => {
@@ -20,9 +21,7 @@ describe('type.js', () => {
 
     it('does not throw an error for recording entity in physio data at root of the dataset', () => {
       expect(
-        type.isBIDS(
-          '/task-matchingpennies_recording-eyetracking_physio.json',
-        ),
+        type.isBIDS('/task-matchingpennies_recording-eyetracking_physio.json'),
       ).toBe(true)
     })
 
@@ -32,6 +31,45 @@ describe('type.js', () => {
         expect(
           type.isBIDS(
             `/sub-05/${mod}/sub-05_task-matchingpennies_recording-eyetracking_physio.tsv.gz`,
+          ),
+        ).toBe(true)
+      })
+    })
+  })
+  describe('schema mode tests', () => {
+    beforeAll(async () => {
+      const schema = await schemaRegex('v1.6.0', { local: true })
+      schemaSetup(schema)
+    })
+    afterAll(() => {
+      schemaSetup()
+    })
+    describe('isAnat()', () => {
+      it('does not throw an error when matching basic anat data files', () => {
+        expect(type.file.isAnat('/sub-01/anat/sub-01_T1w.nii.gz')).toBe(true)
+      })
+    })
+    describe('isBIDS()', () => {
+      it('does not throw an error for valid defacemask filenames', () => {
+        expect(
+          type.isBIDS(
+            '/sub-rid000043/anat/sub-rid000043_run-02_mod-T1w_defacemask.nii.gz',
+          ),
+        ).toBe(true)
+      })
+
+      it('does not throw an error for recording entity in physio data', () => {
+        expect(
+          type.isBIDS(
+            '/sub-05/eeg/sub-05_task-matchingpennies_recording-eyetracking_physio.tsv.gz',
+          ),
+        ).toBe(true)
+      })
+
+      it('does not throw an error for recording entity in physio data at root of the dataset', () => {
+        expect(
+          type.isBIDS(
+            '/task-matchingpennies_recording-eyetracking_physio.json',
           ),
         ).toBe(true)
       })
