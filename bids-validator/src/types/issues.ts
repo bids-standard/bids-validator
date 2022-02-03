1c25fc418e948df22364b33338b81f9d7e1d95ff
Nell Hardcastle <nell@dev-nell.com>
2022-02-03 07:59:52 -0800

    Add issue types from OpenNeuro

---
+++ bids-validator/src/types/issues.ts
@@ -0,0 +1,35 @@
+const warning = Symbol('warning')
+const error = Symbol('error')
+const ignore = Symbol('ignore')
+type Severity = typeof warning | typeof error | typeof ignore
+
+export interface IssueFileDetail {
+  name: string
+  path: string
+  relativePath: string
+}
+
+export interface IssueFile {
+  key: string
+  code: number
+  file: IssueFileDetail
+  evidence: String
+  line: number
+  character: number
+  severity: Severity
+  reason: string
+  helpUrl: string
+}
+
+/**
+ * Dataset issue, derived from OpenNeuro schema and existing validator implementation
+ */
+export interface Issue {
+  severity: Severity
+  key: string
+  code: number
+  reason: string
+  files: [IssueFile?]
+  additionalFileCount: number
+  helpUrl: string
+}
