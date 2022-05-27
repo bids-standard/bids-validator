893299065be4c27ff7b6ff68d5e5ed0c17a54456
Nell Hardcastle <nell@dev-nell.com>
2022-05-27 16:24:25 -0700

    Simplify the internal Issues types and use OpenNeuro types for output formatting only

--- bids-validator/src/compat/fulltest.ts
+++ bids-validator/src/compat/fulltest.ts
@@ -2,14 +2,9 @@
 import { ValidatorOptions } from '../setup/options.ts'
 import { FileTree } from '../types/filetree.ts'
 import { walkFileTree } from '../schema/walk.ts'
-import { Issue } from '../types/issues.ts'
+import { FullTestIssuesReturn } from '../types/issues.ts'
 import validate from '../../dist/esm/index.js'
 import { AdapterFile } from './adapter-file.ts'
-
-interface FullTestIssuesReturn {
-  errors: Issue[]
-  warnings: Issue[]
-}
 
 export interface FullTestAdapterReturn {
   issues: FullTestIssuesReturn
