121798e9f535b8754a60298999322a9aa9d425c1
Ross Blair <rosswilsonblair@gmail.com>
2022-07-08 16:36:28 -0500

    broken eval functions for the different types of objects we see inside schema rules

--- bids-validator/src/schema/applyRules.ts
+++ bids-validator/src/schema/applyRules.ts
@@ -25,3 +25,69 @@ export function evalCheck(src: string, context: Record<string, any>) {
     console.error(error)
   }
 }
+
+const evalMap = {
+  checks: evalRuleChecks,
+  columns: evalColumns,
+  additional_columns: evalAdditionalColumns,
+  initial_columns: evalInitialColumns,
+  fields: evalJson,
+}
+
+function ruleEval(rule, context) {
+  if (!mapEvalCheck(rule.selectors, context)) {
+    return
+  }
+  Object.keys(rule)
+    .filter((key) => key in evalMap)
+    .map((key) => {
+      evalMap[key](rule, context)
+    })
+}
+
+function mapEvalCheck(statements, context): boolean {
+  return statements.every((x) => evalCheck(x, context))
+}
+
+function evalRuleChecks(rule, context): boolean {
+  if (!mapEvalCheck(rule.checks, context)) {
+    // use rule.issue
+  }
+  return true
+}
+
+function evalColumns(rule, context): void {
+  const headers = Object.keys(context.columns)
+  for (const [ruleHeader, requirement] of Object.entries(rule.columns)) {
+    if (!ruleHeader in headers) {
+      // issue on requirement
+    }
+  }
+}
+
+function evalInitialColumns(rule, context): void {
+  rule.initial_columns.map((ruleHeader, ruleIndex) => {
+    const contextIndex = headers.findIndex(ruleHeader)
+    if (contextIndex === -1) {
+      // this should be caught be required in rule.columns check
+    } else if (ruleIndex !== contextIndex) {
+      // wrong order issue
+    }
+  })
+}
+
+function evalAdditionalColumns(rule, context): void {
+  // hard coding allowed here feels bad
+  if (!rule.additional_columns === 'allowed') {
+    const extraCols = headers.filter((header) => !header in rule.columns)
+    // issue on extra cols
+  }
+}
+
+function evalJsonCheck(rule, context): void {
+  for (const [key, requirement] of Object.entries(rule.fields)) {
+    if (!key in context.sidecar) {
+      // error on requirement.level, requirement.issue
+    }
+  }
+}
