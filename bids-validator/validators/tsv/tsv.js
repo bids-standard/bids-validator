f9d0384969fa62b21c96b7d422166d6057471194
Ross Blair <rosswilsonblair@gmail.com>
2022-07-12 11:06:00 -0500

    add set of three checks against tsv headers.

--- bids-validator/validators/tsv/tsv.js
+++ bids-validator/validators/tsv/tsv.js
@@ -1,6 +1,7 @@
 import Issue from '../../utils/issues/issue'
 import checkAcqTimeFormat from './checkAcqTimeFormat'
 import checkAge89 from './checkAge89'
+import checkHeaders from './checkHeaders'
 import checkStatusCol from './checkStatusCol'
 import checkTypecol from './checkTypeCol'
 import parseTSV from './tsvParser'
@@ -11,7 +12,8 @@ var path = require('path')
  * @param {Array[string]} headers
  * @returns {string}
  */
-const headersEvidence = headers => `Column headers: ${headers.join(', ')}`
+export const headersEvidence = headers =>
+  `Column headers: ${headers.join(', ')}`
 
 /**
  * Format TSV filename for evidence string
@@ -51,6 +53,8 @@ const TSV = (file, contents, fileList, callback) => {
   let columnMismatch = false
   let emptyCells = false
   let NACells = false
+
+  checkHeaders(headers, file, issues)
 
   for (let i = 0; i < rows.length; i++) {
     const values = rows[i]
