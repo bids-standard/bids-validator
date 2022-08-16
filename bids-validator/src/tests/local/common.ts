861577f0482075cdc24d8ab0c10bc08cd6d77942
Nell Hardcastle <nell@dev-nell.com>
2022-08-16 10:40:39 -0700

    tests: Improve logging for tests to show the actual failing Issue
    Adds formatAssertIssue function for tests to log the failing Issue object easily

--- bids-validator/src/tests/local/common.ts
+++ bids-validator/src/tests/local/common.ts
@@ -2,6 +2,7 @@ import { readFileTree } from '../../files/deno.ts'
 import { FileTree } from '../../types/filetree.ts'
 import { validate } from '../../validators/bids.ts'
 import { ValidationResult } from '../../types/validation-result.ts'
+import { Issue } from '../../types/issues.ts'
 import { DatasetIssues } from '../../issues/datasetIssues.ts'
 import { Summary } from '../../summary/summary.ts'
 
@@ -25,4 +26,12 @@ export async function validatePath(
   })
 
   return { tree, result }
+}
+
+export function formatAssertIssue(message: string, issue?: Issue) {
+  if (issue) {
+    return `${message}\n${Deno.inspect(issue, { depth: 8, colors: true })}`
+  } else {
+    return `${message}\nAsserted issue is undefined`
+  }
 }
