5915f9541da3e559a81df21c41108ea7931010e9
Ross Blair <rosswilsonblair@gmail.com>
2022-09-23 14:39:24 -0500

    Update bids-validator/src/files/nifti.ts
    Co-authored-by: Nell Hardcastle <nell@dev-nell.com>

--- bids-validator/src/files/nifti.ts
+++ bids-validator/src/files/nifti.ts
@@ -1,7 +1,7 @@
 import 'https://raw.githubusercontent.com/rii-mango/NIFTI-Reader-JS/master/release/current/nifti-reader-min.js'
 import { BIDSFileDeno } from './deno.ts'
 
-export function loadHeader(file: BIDSFile) {
+export function loadHeader(file: BIDSFileDeno) {
   const header = nifti.readHeader(Deno.readFileSync(file._getPath()).buffer)
   // normalize between nifti-reader and spec schema
   // https://github.com/bids-standard/bids-specification/blob/master/src/schema/meta/context.yaml#L200
