18d0e0887b0f834beb8284867af475dc654bf4c5
Ross Blair <rosswilsonblair@gmail.com>
2023-01-31 13:57:32 -0600

    set inspect depth back to 6

--- bids-validator/src/main.ts
+++ bids-validator/src/main.ts
@@ -8,7 +8,7 @@ import { consoleFormat } from './utils/output.ts'
 function inspect(obj: any) {
   console.log(
     Deno.inspect(obj, {
-      depth: 8,
+      depth: 6,
       colors: true,
     }),
   )
