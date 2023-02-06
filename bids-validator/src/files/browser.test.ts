f9b92dfb6fc65afda6ca8b05b054b6c7ded7d992
Nell Hardcastle <nell@dev-nell.com>
2023-02-06 12:26:05 -0800

    feat(schema): Add browser compatible file API
    TODO is test issue and doesn't handle bidsignore

---
+++ bids-validator/src/files/browser.test.ts
@@ -0,0 +1,68 @@
+import { FileIgnoreRules } from './ignore.ts'
+import { FileTree } from '../types/filetree.ts'
+import { assertEquals } from '../deps/asserts.ts'
+import { BIDSFileBrowser, fileListToTree } from './browser.ts'
+
+class TestFile extends File {
+  webkitRelativePath: string
+  constructor(
+    fileBits: BlobPart[],
+    fileName: string,
+    webkitRelativePath: string,
+    options?: FilePropertyBag | undefined,
+  ) {
+    super(fileBits, fileName, options)
+    this.webkitRelativePath = webkitRelativePath
+  }
+}
+
+Deno.test('Browser implementation of FileTree', async (t) => {
+  await t.step('converts a basic FileList', async () => {
+    const ignore = new FileIgnoreRules([])
+    const files = [
+      new TestFile(
+        ['{}'],
+        'dataset_description.json',
+        'dataset_description.json',
+      ),
+      new TestFile(['flat test dataset'], 'README.md', 'README.md'),
+    ]
+    const tree = await fileListToTree(files)
+    const expectedTree = new FileTree('', '/', undefined)
+    expectedTree.files = files.map((f) => new BIDSFileBrowser(f, ignore))
+    assertEquals(tree, expectedTree)
+  })
+  await t.step('converts a simple FileList with several levels', async () => {
+    const ignore = new FileIgnoreRules([])
+    const files = [
+      new TestFile(
+        ['{}'],
+        'dataset_description.json',
+        'dataset_description.json',
+      ),
+      new TestFile(
+        ['tsv headers\n', 'column\tdata'],
+        'participants.tsv',
+        'participants.tsv',
+      ),
+      new TestFile(['single subject test dataset'], 'README.md', 'README.md'),
+      new TestFile(
+        ['nifti file goes here'],
+        'sub-01_T1w.nii.gz',
+        'sub-01/anat/sub-01_T1w.nii.gz',
+      ),
+    ]
+    const tree = await fileListToTree(files)
+    const expectedTree = new FileTree('', '/', undefined)
+    const sub01Tree = new FileTree('sub-01', 'sub-01', expectedTree)
+    const anatTree = new FileTree('sub-01/anat', 'anat', sub01Tree)
+    expectedTree.files = files
+      .slice(0, 3)
+      .map((f) => new BIDSFileBrowser(f, ignore))
+    expectedTree.directories.push(sub01Tree)
+    anatTree.files = [new BIDSFileBrowser(files[3], ignore)]
+    sub01Tree.directories.push(anatTree)
+    // TODO - Stack size exceeded
+    assertEquals(tree, expectedTree)
+  })
+})
---
+++ bids-validator/src/files/browser.ts
@@ -0,0 +1,88 @@
+import { BIDSFile } from '../types/file.ts'
+import { FileTree } from '../types/filetree.ts'
+import { FileIgnoreRules } from './ignore.ts'
+import { parse, join, SEP } from '../deps/path.ts'
+
+/**
+ * Browser implement of BIDSFile wrapping native File/FileList types
+ */
+export class BIDSFileBrowser implements BIDSFile {
+  #ignore: FileIgnoreRules
+  #file: File
+
+  constructor(file: File, ignore: FileIgnoreRules) {
+    this.#file = file
+    this.#ignore = ignore
+  }
+
+  get name(): string {
+    return this.#file.name
+  }
+
+  get path(): string {
+    // @ts-expect-error webkitRelativePath is defined in the browser
+    return this.#file.webkitRelativePath
+  }
+
+  get size(): number {
+    return this.#file.size
+  }
+
+  get stream(): ReadableStream<Uint8Array> {
+    return this.#file.stream()
+  }
+
+  get ignored(): boolean {
+    return this.#ignore.test(this.path)
+  }
+
+  text(): Promise<string> {
+    return this.#file.text()
+  }
+
+  readBytes(size: number, offset = 0): Uint8Array {
+    // TODO - Promise or FileReaderSync?
+    // @ts-ignore obviously
+    return this.#file.slice(offset, size)
+  }
+}
+
+/**
+ * Convert from FileList (created with webkitDirectory: true) to FileTree for validator use
+ */
+export function fileListToTree(files: File[]): Promise<FileTree> {
+  const ignore = new FileIgnoreRules([])
+  const tree = new FileTree('', '/', undefined)
+  for (const f of files) {
+    const file = new BIDSFileBrowser(f, ignore)
+    const fPath = parse(file.path)
+    const levels = fPath.dir.split(SEP)
+    if (levels[0] === '') {
+      // Top level file
+      tree.files.push(file)
+    } else {
+      let currentLevelTree = tree
+      for (const level of levels) {
+        const exists = currentLevelTree.directories.find(
+          (d) => d.name === level,
+        )
+        // If this level exists, set it and descend once
+        if (exists) {
+          currentLevelTree = exists
+        } else {
+          // Otherwise make a new level and continue if needed
+          const newTree = new FileTree(
+            join(currentLevelTree.path, level),
+            level,
+            currentLevelTree,
+          )
+          currentLevelTree.directories.push(newTree)
+          currentLevelTree = newTree
+        }
+      }
+      // At the terminal leaf, add files
+      currentLevelTree.files.push(file)
+    }
+  }
+  return Promise.resolve(tree)
+}
