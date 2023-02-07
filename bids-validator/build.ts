d99bbf24b85dcd15e33c0f31b5ccffb791c80ca6
Nell Hardcastle <nell@dev-nell.com>
2023-02-07 12:31:06 -0800

    fix(schema): Support http/https resolution of js,ts,json files at build time

--- bids-validator/build.ts
+++ bids-validator/build.ts
@@ -9,6 +9,33 @@ import { parse } from 'https://deno.land/std@0.175.0/flags/mod.ts'
 
 const MAIN_ENTRY = 'src/main.ts'
 
+const httpPlugin = {
+  name: 'http',
+  setup(build: esbuild.PluginBuild) {
+    build.onResolve({ filter: /^https?:\/\// }, (args) => ({
+      path: args.path,
+      namespace: 'http-url',
+    }))
+
+    build.onResolve({ filter: /.*/, namespace: 'http-url' }, (args) => ({
+      path: new URL(args.path, args.importer).toString(),
+      namespace: 'http-url',
+    }))
+
+    build.onLoad({ filter: /.*/, namespace: 'http-url' }, async (args) => {
+      const request = await fetch(args.path)
+      const contents = await request.text()
+      if (args.path.endsWith('.ts')) {
+        return { contents, loader: 'ts' }
+      } else if (args.path.endsWith('.json')) {
+        return { contents, loader: 'json' }
+      } else {
+        return { contents, loader: 'js' }
+      }
+    })
+  },
+}
+
 const flags = parse(Deno.args, {
   boolean: ['minify'],
   default: { minify: false },
@@ -20,6 +47,9 @@ const result = await esbuild.build({
   bundle: true,
   outdir: 'dist/validator',
   minify: flags.minify,
+  target: ['chrome109', 'firefox109', 'safari16'],
+  plugins: [httpPlugin],
+  allowOverwrite: true,
 })
 
 if (result.warnings.length > 0) {
