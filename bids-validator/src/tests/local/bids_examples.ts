a8640f6cf5f96c37554faf828518f27248b1c27d
Ross Blair <rosswilsonblair@gmail.com>
2023-02-07 13:01:34 -0600

    track full path of schema rules and try and get it into issues when they're created

--- bids-validator/src/tests/local/bids_examples.ts
+++ bids-validator/src/tests/local/bids_examples.ts
@@ -5,8 +5,20 @@ import { validatePath, formatAssertIssue } from './common.ts'
 
 const options = { ignoreNiftiHeaders: true }
 
-function formatBEIssue(issue: IssueOutput) {
-  return `${issue.key}: ${issue.files[0].evidence} ${issue.files[0].file.name}`
+function useIssue(issue: IssueOutput): boolean {
+  return (
+    'schema.rules.checks.general.DuplicateFiles' !== issue.files[0].evidence &&
+    issue.key !== 'EMPTY_FILE'
+  )
+}
+
+const errors: string[] = ['dataset\tissueKey\tschemaPath\tfileName\n']
+function formatBEIssue(issue: IssueOutput, dsPath: string) {
+  if (useIssue(issue)) {
+    errors.push(
+      `${dsPath}\t${issue.key}\t${issue.files[0].evidence}\t${issue.files[0].file.name}\n`,
+    )
+  }
 }
 
 Deno.test('validate bids-examples', async (t) => {
@@ -18,9 +30,11 @@ Deno.test('validate bids-examples', async (t) => {
     const path = `${prefix}/${dirEntry.name}`
     const { tree, result } = await validatePath(t, path, options)
     const output = result.issues.formatOutput()
-    const errorMessages = output.errors.map((x) => formatBEIssue(x))
+    output.errors = output.errors.filter((x) => useIssue(x))
+    output.errors.map((x) => formatBEIssue(x, dirEntry.name))
     await t.step(`${path} has no issues`, () => {
-      assertEquals(output.errors.length, 0, Deno.inspect(errorMessages))
+      assertEquals(output.errors.length, 0)
     })
   }
+  Deno.writeTextFile('./bids_examples_issues.tsv', errors.join(''))
 })
