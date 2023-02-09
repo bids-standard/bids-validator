131af771b5344d8f5e38b6f9c1efab658b1e1708
Ross Blair <rosswilsonblair@gmail.com>
2023-02-09 11:18:22 -0600

    what if test failures, but colorful

--- bids-validator/src/tests/schema-expression-language.test.ts
+++ bids-validator/src/tests/schema-expression-language.test.ts
@@ -1,5 +1,6 @@
 import { loadSchema } from '../setup/loadSchema.ts'
-import { Cell, Row, Table } from '../deps/cliffy.ts'
+import { Table } from '../deps/cliffy.ts'
+import { colors } from '../deps/fmt.ts'
 import { BIDSContext } from '../schema/context.ts'
 import { assert, assertEquals } from '../deps/asserts.ts'
 import { evalCheck } from '../schema/applyRules.ts'
@@ -9,23 +10,25 @@ const pretty_null = (x: string | null): string => (x === null ? 'null' : x)
 
 Deno.test('validate schema expression tests', async (t) => {
   const results: string[][] = []
-  const header = ['expression', 'desired', 'actual', 'result']
+  const header = ['expression', 'desired', 'actual', 'result'].map((x) =>
+    colors.magenta(x),
+  )
   for (const test of schema.meta.expression_tests) {
     await t.step(`${test.expression} evals to ${test.result}`, () => {
       const actual_result = evalCheck(test.expression, {} as BIDSContext)
       if (actual_result == test.result) {
         results.push([
-          test.expression,
+          colors.cyan(test.expression),
           pretty_null(test.result),
           pretty_null(actual_result),
-          'pass',
+          colors.green('pass'),
         ])
       } else {
         results.push([
-          test.expression,
+          colors.cyan(test.expression),
           pretty_null(test.result),
           pretty_null(actual_result),
-          'fail',
+          colors.red('fail'),
         ])
       }
       assertEquals(actual_result, test.result)
