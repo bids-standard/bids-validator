d208fa02b8d7165356157cec3b491690d8bcd055
Ross Blair <rosswilsonblair@gmail.com>
2023-02-09 13:18:03 -0600

    fix typescript in schema.object lookup

--- bids-validator/src/schema/applyRules.ts
+++ bids-validator/src/schema/applyRules.ts
@@ -207,9 +207,9 @@ function evalAdditionalColumns(
   if (context.extension !== '.tsv') return
   const headers = Object.keys(context?.columns)
   // hard coding allowed here feels bad
-  if (!(rule.additional_columns === 'allowed')) {
-    // @ts-expect-error
+  if (!(rule.additional_columns === 'allowed') && rule.columns) {
     const ruleHeadersNames = Object.keys(rule.columns).map(
+      // @ts-expect-error
       (x) => schema.objects.columns[x].name,
     )
     let extraCols = headers.filter(
