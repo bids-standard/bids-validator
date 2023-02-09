406f8ff2303e668cb51f665f70d4ac699ef79764
Nell Hardcastle <nell@dev-nell.com>
2023-02-09 09:27:57 -0800

    feat(schema): Automatically log the caller location at DEBUG level

--- bids-validator/src/utils/logger.ts
+++ bids-validator/src/utils/logger.ts
@@ -1,4 +1,10 @@
-import { setup, handlers, LevelName, getLogger } from '../deps/logger.ts'
+import {
+  setup,
+  handlers,
+  LevelName,
+  getLogger,
+  Logger,
+} from '../deps/logger.ts'
 
 /**
  * Setup a console logger used with the --debug flag
@@ -18,6 +24,19 @@ export function setupLogging(level: LevelName) {
   })
 }
 
-const logger = getLogger('@bids/validator')
+const loggerProxyHandler = {
+  get: function (_: void, prop: keyof Logger) {
+    const logger = getLogger('@bids/validator')
+    const stack = new Error().stack
+    if (stack) {
+      const callerLocation = stack.split('\n')[2].trim().split(' ')[1]
+      logger.debug(`Logger invoked at "${callerLocation}"`)
+    }
+    const logFunc = logger[prop] as typeof logger.warning
+    return logFunc.bind(logger)
+  },
+}
+
+const logger = new Proxy(getLogger('@bids/validator'), loggerProxyHandler)
 
 export { logger }
