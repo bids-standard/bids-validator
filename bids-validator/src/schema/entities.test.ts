861222ed90841d3c837b39ef59cd6d5820148db8
Nell Hardcastle <nell@dev-nell.com>
2023-05-17 13:36:43 -0700

    fix(schema): Add memoization and minor optimizations to readEntities
    Most of the benefit is from memoization but saves a little extra time on object references

--- bids-validator/src/schema/entities.test.ts
+++ bids-validator/src/schema/entities.test.ts
@@ -1,9 +1,9 @@
-import { assert, assertEquals } from '../deps/asserts.ts'
-import { BIDSFile } from '../types/file.ts'
+import { assert } from '../deps/asserts.ts'
 import { readEntities } from './entities.ts'
 import { nullReadBytes } from '../tests/nullReadBytes.ts'
+import { generateBIDSFilename } from '../tests/generate-filenames.ts'
 
-Deno.test('test readEntities', async (t) => {
+Deno.test('test readEntities', (t) => {
   const testFile = {
     name: 'task-rhymejudgment_bold.json',
     path: '/task-rhymejudgment_bold.json',
@@ -13,8 +13,29 @@ Deno.test('test readEntities', async (t) => {
     text: () => Promise.resolve(''),
     readBytes: nullReadBytes,
   }
-  const context = readEntities(testFile)
+  const context = readEntities(testFile.name)
   assert(context.suffix === 'bold', 'failed to match suffix')
   assert(context.extension === '.json', 'failed to match extension')
   assert(context.entities.task === 'rhymejudgment', 'failed to match extension')
+})
+
+Deno.test('test readEntities performance', (t) => {
+  const generateStart = performance.now()
+  const testFilenames = []
+  for (let n = 0; n < 200000; n++) {
+    testFilenames.push(generateBIDSFilename(Math.floor(Math.random() * 4)))
+  }
+  const generateEnd = performance.now()
+  const normalizePerf = generateEnd - generateStart
+
+  const start = performance.now()
+  for (const each of testFilenames) {
+    readEntities(each)
+  }
+  const end = performance.now()
+  const readEntitiesTime = end - start
+
+  const perfRatio = readEntitiesTime / normalizePerf + Number.EPSILON
+  console.log(`readEntities() runtime ratio: ${perfRatio.toFixed(2)}`)
+  assert(perfRatio < 2)
 })
