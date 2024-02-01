ce462d8f06a952db4465762dfb71d5c9b37ad973
Nell Hardcastle <nell@dev-nell.com>
2024-02-01 11:29:54 -0800

    deps(deno): Update to latest standard library and cliffy

--- bids-validator/src/deps/logger.ts
+++ bids-validator/src/deps/logger.ts
@@ -1,14 +1,14 @@
 export {
-  Logger,
-  LogLevels,
-  error,
   critical,
   debug,
+  error,
+  getLogger,
   info,
+  Logger,
+  LogLevels,
   setup,
   warning,
-  handlers,
-  getLogger,
-} from 'https://deno.land/std@0.177.0/log/mod.ts'
-export { LogLevelNames } from 'https://deno.land/std@0.177.0/log/levels.ts'
-export type { LevelName } from 'https://deno.land/std@0.177.0/log/mod.ts'
+} from "std/log/mod.ts"
+export { ConsoleHandler } from "std/log/console_handler.ts"
+export { LogLevelNames } from "std/log/levels.ts"
+export type { LevelName } from "std/log/mod.ts"
