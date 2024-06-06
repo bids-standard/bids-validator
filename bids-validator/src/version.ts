ec36ec5518848013df68b540507129ed0d9d6127
Chris Markiewicz <effigies@gmail.com>
2024-06-06 16:26:42 -0400

    feat(version): Find version from git archive

--- bids-validator/src/version.ts
+++ bids-validator/src/version.ts
@@ -1,6 +1,12 @@
+import gitmeta from './.git-meta.json' with { type: 'json' }
+
 export async function getVersion(): string {
   const url = import.meta.url
   if (url.startsWith('file://')) {
+    const archiveVersion = getArchiveVersion()
+    if (archiveVersion) {
+      return archiveVersion
+    }
     // Get parent directory of current file, without file:/
     const parent = url.slice(7).split('/').slice(0, -1).join('/')
     return await getLocalVersion(parent)
@@ -14,7 +20,7 @@ export async function getVersion(): string {
   return url
 }
 
-export async function getLocalVersion(path: string): string {
+async function getLocalVersion(path: string): string {
   const p = Deno.run({
     cmd: ['git', 'describe', '--tags', '--always'],
     stdout: 'piped',
@@ -23,4 +29,11 @@ export async function getLocalVersion(path: string): string {
   })
   const description = new TextDecoder().decode(await p.output()).trim()
   return description
+}
+
+function getArchiveVersion(): string | undefined {
+  if (!gitmeta.description.startsWith('$Format:')) {
+    return gitmeta.description
+  }
+  return undefined
 }
