9bba8a49f1172e7bdb155a5d70b19743c09a5c0b
Chris Markiewicz <effigies@gmail.com>
2024-06-10 08:11:48 -0400

    fix: Import posix mod separately, as path.posix is deprecated

--- bids-validator/src/deps/path.ts
+++ bids-validator/src/deps/path.ts
@@ -5,7 +5,6 @@ export {
   basename,
   dirname,
   extname,
-  posix,
   fromFileUrl,
   parse,
   SEPARATOR_PATTERN,
@@ -13,3 +12,4 @@ export {
 export {
   globToRegExp,
 } from 'https://deno.land/std@0.217.0/path/glob_to_regexp.ts'
+export * as posix from 'https://deno.land/std@0.217.0/path/posix/mod.ts'
