6bec9920098b9b65ac1b11542c9b10ffe9086a47
Chris Markiewicz <effigies@gmail.com>
2024-07-10 17:29:44 -0400

    test(legacy): Check whether polluting objects with a global property breaks things

---
+++ bids-validator/utils/files/__tests__/generateMergedSidecarDict.spec.js
@@ -0,0 +1,44 @@
+import generateMergedSidecarDict from '../generateMergedSidecarDict.js'
+
+describe('generateMergedSidecarDict.js', () => {
+  describe('Object pollution test', () => {
+    beforeAll(() => {
+      // Simulate code that injects globalThis into every object
+      Object.defineProperty(Object.prototype, 'global', {
+        get: function () {
+          return globalThis
+        },
+        configurable: true,
+      })
+    })
+
+    afterAll(() => {
+      // Clean up the pollution
+      delete Object.prototype.global
+    })
+
+    it('merges objects with global property', () => {
+      const potentialSidecars = ['/sidecar1.json', '/sidecar2.json']
+      const jsonContents = {
+        '/sidecar1.json': {
+          RegularMetadata1: 'value1',
+          global: {
+            globalMetadata: 'value1',
+          },
+        },
+        '/sidecar2.json': {
+          RegularMetadata2: 'value2',
+        },
+      }
+      expect(
+        generateMergedSidecarDict(potentialSidecars, jsonContents),
+      ).toEqual({
+        RegularMetadata1: 'value1',
+        RegularMetadata2: 'value2',
+        global: {
+          globalMetadata: 'value1',
+        },
+      })
+    })
+  })
+})
