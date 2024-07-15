5b00b5fa1344636177aeb90b8e86654f09e98d7b
Ross Blair <rosswilsonblair@gmail.com>
2024-07-15 14:16:37 -0500

    put all ajv exports in a single line with import type modifier as needed

--- bids-validator/src/deps/ajv.ts
+++ bids-validator/src/deps/ajv.ts
@@ -1,4 +1,1 @@
-export {Ajv} from 'https://esm.sh/ajv@8.16.0';
-import ValidateFunction from 'https://esm.sh/ajv@8.16.0';
-import JSONSchemaType from 'https://esm.sh/ajv@8.16.0';
-export { ValidateFunction, JSONSchemaType }
+export {Ajv, type ValidateFunction, type JSONSchemaType } from 'https://esm.sh/ajv@8.16.0';
