d0db3092f6dba26bd9425b6b13e614bb72c9f621
Ross Blair <rosswilsonblair@gmail.com>
2024-07-30 14:36:04 -0500

    overhaul integration tests for hed validate

--- bids-validator/src/tests/local/hed-integration.test.ts
+++ bids-validator/src/tests/local/hed-integration.test.ts
@@ -1,36 +1,63 @@
+import { formatAssertIssue, validatePath } from './common.ts'
 import { assert, assertEquals } from '../../deps/asserts.ts'
-import { validatePath, formatAssertIssue } from './common.ts'
-import { validate } from '../../validators/bids.ts'
-import { parseOptions } from '../../setup/options.ts'
-import { testDetectHed } from '../../validators/hed.ts'
-import { BIDSContextDataset } from '../../schema/context.ts'
+import { BIDSFileDeno, readFileTree } from '../../files/deno.ts'
+import { DatasetIssues } from '../../issues/datasetIssues.ts'
+import { loadSchema } from '../../setup/loadSchema.ts'
+import { BIDSContext, BIDSContextDataset } from '../../schema/context.ts'
+import { BIDSFile } from '../../types/file.ts'
+import { FileTree } from '../../types/filetree.ts'
+import { GenericSchema } from '../../types/schema.ts'
+import { hedValidate } from '../../validators/hed.ts'
+
+function getFile(fileTree: FileTree, path: string) {
+  let [current, ...nextPath] = path.split('/')
+  if (nextPath.length === 0) {
+    const target = fileTree.files.find(x => x.name === current)
+    if (target) {
+      return target
+    }
+    const dirTarget = fileTree.directories.find(x => x.name === nextPath[0]) 
+    return dirTarget
+  } else {
+    const nextTree = fileTree.directories.find(x => x.name === current)
+    if (nextTree) {
+      return getFile(nextTree, nextPath.join('/'))
+    }
+  }
+  return undefined
+}
 
 Deno.test('hed-validator not triggered', async (t) => {
   const PATH = 'tests/data/bids-examples/ds003'
-  const { tree, result } = await validatePath(t, PATH)
-
+  const tree = await readFileTree(PATH)
+  const schema = await loadSchema()
+  const issues = new DatasetIssues()
+  const dsContext = new BIDSContextDataset(undefined, {'HEDVersion': ['bad_version']})
   await t.step('detect hed returns false', async () => {
-    const new_result = await validate(tree, {
-      ...(await parseOptions([PATH]))
-    })
-
-    // Dummy dscontext until I can figure out how to extract a good one from validate
-    const dsContext = new BIDSContextDataset()
-    assert(!testDetectHed(dsContext))
+    const eventFile = getFile(tree, 'sub-01/func/sub-01_task-rhymejudgment_events.tsv')
+    assert(eventFile !== undefined)
+    assert(eventFile instanceof BIDSFileDeno)
+    const context = new BIDSContext(tree, eventFile, issues, dsContext)
+    await context.asyncLoads()
+    await hedValidate(schema as unknown as GenericSchema, context)
+    assert(issues.size === 0)
   })
 })
 
-Deno.test('hed-validator integration test', async (t) => {
+Deno.test('hed-validator fails with bad schema version', async (t) => {
   const PATH = 'tests/data/bids-examples/eeg_ds003645s_hed_library'
-  const { tree, result } = await validatePath(t, PATH)
-
-  tree.files = tree.files.filter(f => f.name !== 'task-FacePerception_events.json')
-  await t.step('generates hed error on missing events json', async () => {
-    const new_result = await validate(tree, {
-      ...(await parseOptions([PATH]))
-    })
-    assert(new_result.issues.has("HED_ERROR"))
+  const tree = await readFileTree(PATH)
+  const schema = await loadSchema()
+  const issues = new DatasetIssues()
+  const dsContext = new BIDSContextDataset(undefined, {'HEDVersion': ['bad_version']})
+  await t.step('detect hed returns false', async () => {
+    const eventFile = getFile(tree, 'sub-002/eeg/sub-002_task-FacePerception_run-3_events.tsv')
+    assert(eventFile !== undefined)
+    assert(eventFile instanceof BIDSFileDeno)
+    const context = new BIDSContext(tree, eventFile, issues, dsContext)
+    await context.asyncLoads()
+    await hedValidate(schema as unknown as GenericSchema, context)
+    assert(issues.size === 1)
+    assert(issues.has('HED_ERROR'))
   })
 })
-
-
