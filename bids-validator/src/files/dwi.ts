11a4417d42d16533d53891ee32b4720484122bc2
Chris Markiewicz <effigies@gmail.com>
2024-08-02 14:03:01 -0400

    fix(bvec): Improve empty row detection, stop preemptive numeric conversion

--- bids-validator/src/files/dwi.ts
+++ bids-validator/src/files/dwi.ts
@@ -4,18 +4,13 @@
  */
 const normalizeEOL = (str: string): string => str.replace(/\r\n/g, '\n').replace(/\r/g, '\n')
 
-export function parseBvalBvec(contents: string): number[][] {
+export function parseBvalBvec(contents: string): string[][] {
   // BVEC files are a matrix of numbers, with each row being
   // a different axis
   // BVAL files are a single row of numbers, and may contain
   // trailing whitespace
   return normalizeEOL(contents)
-    .split(/\s*\n/)
-    .filter((x) => x !== '')
-    .map((row) =>
-      row
-        .split(/\s+/)
-        .filter((x) => x !== '')
-        .map((x) => Number(x))
-    )
+    .split(/\s*\n/) // Split on newlines, ignoring trailing whitespace
+    .filter((x) => x.match(/\S/)) // Remove empty lines
+    .map((row) => row.split(/\s+/))
 }
