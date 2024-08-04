7b4f881ca8ddb8c84746d6d8fa8e6b3082e2639a
Chris Markiewicz <effigies@gmail.com>
2024-08-04 13:35:01 -0400

    test(nifti): Check header details, update ContextNiftiHeader interface

--- bids-validator/src/files/nifti.test.ts
+++ bids-validator/src/files/nifti.test.ts
@@ -6,14 +6,43 @@ import { loadHeader } from './nifti.ts'
 
 Deno.test('Test loading nifti header', async (t) => {
   const ignore = new FileIgnoreRules([])
-  await t.step('Load header from compressed file', async () => {
+
+  await t.step('Load header from compressed 3D file', async () => {
+    const path = 'sub-01/anat/sub-01_T1w.nii.gz'
+    const root = './tests/data/valid_headers'
+    const file = new BIDSFileDeno(root, path, ignore)
+    const header = await loadHeader(file)
+    assert(header !== undefined)
+    assertObjectMatch(header, {
+      dim: [3, 40, 48, 48, 1, 1, 1, 1],
+      shape: [40, 48, 48],
+      dim_info: { freq: 0, phase: 0, slice: 0 },
+      xyzt_units: { xyz: 'mm', t: 'sec' },
+      qform_code: 1,
+      sform_code: 1,
+    })
+    // Annoying floating point precision, skipping pixdim details
+    assert(header.voxel_sizes.length === 3)
+  })
+
+  await t.step('Load header from compressed 4D file', async () => {
     const path = 'sub-01/func/sub-01_task-rhymejudgment_bold.nii.gz'
     const root = './tests/data/valid_headers'
     const file = new BIDSFileDeno(root, path, ignore)
     const header = await loadHeader(file)
     assert(header !== undefined)
-    assert(header['pixdim'].length === 8)
+    assertObjectMatch(header, {
+      dim: [4, 16, 16, 9, 20, 1, 1, 1],
+      pixdim: [0, 12.5, 12.5, 16, 1, 0, 0, 0],
+      shape: [16, 16, 9, 20],
+      voxel_sizes: [12.5, 12.5, 16, 1],
+      dim_info: { freq: 0, phase: 0, slice: 0 },
+      xyzt_units: { xyz: 'mm', t: 'sec' },
+      qform_code: 0,
+      sform_code: 0,
+    })
   })
+
   await t.step('Fail on non-nifti file', async () => {
     const path = 'sub-01/func/sub-01_task-rhymejudgment_events.tsv'
     const root = './tests/data/valid_headers'
