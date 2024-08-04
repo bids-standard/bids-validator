74ae6517b477542cea469944bc24307beaebe51b
Chris Markiewicz <effigies@gmail.com>
2024-08-04 13:18:35 -0400

    test(json): Verify error behavior of JSON

---
+++ bids-validator/src/files/json.test.ts
@@ -0,0 +1,64 @@
+import { assert, assertObjectMatch } from '../deps/asserts.ts'
+import { BIDSFileDeno, UnicodeDecodeError } from './deno.ts'
+import { BIDSFile } from '../types/filetree.ts'
+import { FileIgnoreRules } from './ignore.ts'
+
+import { loadJSON } from './json.ts'
+
+function encodeUTF16(text: string) {
+  // Adapted from https://developer.chrome.com/blog/how-to-convert-arraybuffer-to-and-from-string
+
+  // Ensure BOM is present
+  text = text.charCodeAt(0) === 0xFEFF ? text : '\uFEFF' + text
+  const buffer = new ArrayBuffer(text.length * 2)
+  const view = new Uint16Array(buffer)
+  for (let i = 0, strLen = text.length; i < strLen; i++) {
+    view[i] = text.charCodeAt(i)
+  }
+  return buffer
+}
+
+function makeFile(text: string, encoding: string): BIDSFile {
+  const bytes = encoding === 'utf-8' ? new TextEncoder().encode(text) : encodeUTF16(text)
+  return {
+    readBytes: async (size: number) => {
+      return new Uint8Array(bytes)
+    },
+    size: bytes.byteLength,
+  } as unknown as BIDSFile
+}
+
+Deno.test('Test JSON error conditions', async (t) => {
+  await t.step('Load valid JSON', async () => {
+    const JSONfile = makeFile('{"a": 1}', 'utf-8')
+    const result = await loadJSON(JSONfile)
+    assertObjectMatch(result, { a: 1 })
+  })
+
+  await t.step('Error on BOM', async () => {
+    const BOMfile = makeFile('\uFEFF{"a": 1}', 'utf-8')
+    let error: any = undefined
+    await loadJSON(BOMfile).catch((e) => {
+      error = e
+    })
+    assertObjectMatch(error, { key: 'INVALID_JSON_ENCODING' })
+  })
+
+  await t.step('Error on UTF-16', async () => {
+    const UTF16file = makeFile('{"a": 1}', 'utf-16')
+    let error: any = undefined
+    await loadJSON(UTF16file).catch((e) => {
+      error = e
+    })
+    assertObjectMatch(error, { key: 'INVALID_JSON_ENCODING' })
+  })
+
+  await t.step('Error on invalid JSON syntax', async () => {
+    const badJSON = makeFile('{"a": 1]', 'utf-8')
+    let error: any = undefined
+    await loadJSON(badJSON).catch((e) => {
+      error = e
+    })
+    assertObjectMatch(error, { key: 'JSON_INVALID' })
+  })
+})
--- bids-validator/src/files/json.ts
+++ bids-validator/src/files/json.ts
@@ -1,18 +1,30 @@
 import { BIDSFile } from '../types/filetree.ts'
 
-export async function loadJSON(file: BIDSFile): Promise<Record<string, unknown>> {
-  // Parse JSON more strictly than other files
-  // Do not replace invalid UTF-8 characters or strip the byte order mark (BOM)
-  const text = await file.text({ fatal: true, ignoreBOM: true }).catch((error) => {
+async function readJSONText(file: BIDSFile): Promise<string> {
+  // Read JSON text from a file
+  // JSON must be encoded in UTF-8 without a byte order mark (BOM)
+  const decoder = new TextDecoder('utf-8', { fatal: true, ignoreBOM: true })
+  // Streaming TextDecoders are buggy in Deno and Chrome, so read the
+  // entire file into memory before decoding and parsing
+  const data = await file.readBytes(file.size)
+  try {
+    const text = decoder.decode(data)
+    if (text.startsWith('\uFEFF')) {
+      throw {}
+    }
+    return text
+  } catch (error) {
     throw { key: 'INVALID_JSON_ENCODING' }
-  })
-  // A BOM would cause a syntax error in JSON.parse, but we treat it as an encoding error
-  if (text.match(/^\uFEFF/)) {
-    throw { key: 'INVALID_JSON_ENCODING' }
+  } finally {
+    decoder.decode() // Reset decoder
   }
+}
+
+export async function loadJSON(file: BIDSFile): Promise<Record<string, unknown>> {
+  const text = await readJSONText(file) // Raise encoding errors
   try {
     return JSON.parse(text)
   } catch (error) {
-    throw { key: 'JSON_INVALID' }
+    throw { key: 'JSON_INVALID' } // Raise syntax errors
   }
 }
