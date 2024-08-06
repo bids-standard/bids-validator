a57dda991f69ae826a18e70fd4e3807862f63be1
Chris Markiewicz <effigies@gmail.com>
2024-08-06 10:34:10 -0400

    chore: Remove Python library

--- bids-validator/bids_validator/__init__.py
+++
@@ -1,6 +0,0 @@
-"""BIDS validator common Python package."""
-from .bids_validator import BIDSValidator
-__all__ = ['BIDSValidator']
-
-from . import _version
-__version__ = _version.get_versions()['version']
--- bids-validator/setup.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup
-import versioneer
-
-setup(version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass())
