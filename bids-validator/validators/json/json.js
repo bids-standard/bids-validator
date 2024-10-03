51981968f710af7779c726d1b81182aca1d4c4aa
Ross Blair <rosswilsonblair@gmail.com>
2024-10-03 12:22:45 -0500

    strictSchema to false

--- bids-validator/validators/json/json.js
+++ bids-validator/validators/json/json.js
@@ -1,6 +1,8 @@
 import utils from '../../utils'
 import Ajv from 'ajv'
-const ajv = new Ajv({ allErrors: true })
+import addFormats from "ajv-formats"
+const ajv = new Ajv({ allErrors: true, strictSchema: false })
+addFormats(ajv)
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'))
 ajv.addSchema(require('./schemas/common_definitions.json'))
 const Issue = utils.issues.Issue
