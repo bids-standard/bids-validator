80b27894d33dfdddb37eb5aa83dc5df7483c3dac
Ross Blair <rosswilsonblair@gmail.com>
2024-10-14 14:50:22 -0500

    bump versions for esbuild and deno-loader in deno build script

--- bids-validator/build.ts
+++ bids-validator/build.ts
@@ -4,9 +4,9 @@
  *
  * If you would like to use this package in a Node.js project, you'll need to use native ESM or a transform system
  */
-import * as esbuild from 'https://deno.land/x/esbuild@v0.20.2/mod.js'
+import * as esbuild from 'https://deno.land/x/esbuild@v0.24.0/mod.js'
 import { parse } from 'https://deno.land/std@0.223.0/flags/mod.ts'
-import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.10"
+import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.11.0"
 import * as path from "https://deno.land/std@0.223.0/path/mod.ts"
 import { getVersion } from './src/version.ts'
 
