925dc21f0db0d82683726e0e9c72178cb39b3417
Ross Blair <rosswilsonblair@gmail.com>
2024-11-07 15:47:42 -0600

    Show a message on no errors or warnings

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react"
 import "./App.css"
 import { directoryOpen } from "https://esm.sh/browser-fs-access@0.35.0"
+import confetti, { create } from 'https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.module.mjs';
 import { fileListToTree, validate } from "../dist/validator/main.js"
 import type { ValidationResult } from "../../src/types/validation-result.ts"
 import { Collapse } from "./Collapse.tsx"
 import { Summary } from "./Summary.tsx"
+console.log(confetti)
 
 function Files({ issues }) {
   const unique = new Map(issues.map(({ location, issueMessage }) => {
@@ -61,6 +63,7 @@ function App() {
   if (validation) {
     const errorList = []
     const warningList = []
+    let success = ""
     for (const [code, issues] of validation.issues.filter({ severity: 'error' }).groupBy('code')) {
       if (code === 'None') {
         continue
@@ -73,6 +76,20 @@ function App() {
       }
       warningList.push(...Issue({ data: issues }))
     }
+    if (errorList.length === 0 && warningList.length === 0) {
+      success = (
+      <div className="success">
+        <Collapse label="No Issues Detected!">
+          <div>Thanks for Everything. I Have No Complaints Whatsoever.</div>
+        </Collapse>
+      </div>
+      )
+      confetti({
+        particleCount: 500,
+        spread: 180,
+        ticks: 400
+      });
+    }
     validatorOutput = (
       <>
         <button onClick={validateDir}>Reselect Files</button>
@@ -80,6 +97,7 @@ function App() {
           <ul className="issues-list">
             {errorList}
             {warningList}
+            {success}
           </ul>
           <Summary data={validation.summary} />
           <a
@@ -92,7 +110,10 @@ function App() {
         </div>
       </>
     )
+    if (errorList.length === 0) {
+    }
   } else {
+
     validatorOutput = (
       <>
         <h2>
@@ -106,7 +127,6 @@ function App() {
       </>
     )
   }
-
   return (
     <>
       <h1>BIDS Validator</h1>
