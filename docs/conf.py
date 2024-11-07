17bcda5990e193cedd78dcaba7b61d3cab4b7fae
Chris Markiewicz <effigies@gmail.com>
2024-11-06 20:33:16 -0500

    Initial docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,11 @@ author = 'BIDS Contributors'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    # 'sphinx_js',
+    'myst_parser',
+    'sphinx_copybutton',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +27,22 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    "logo": {
+        "text": "BIDS Validator",
+        "image_light": "_static/BIDS_black.svg",
+        "image_dark": "_static/BIDS_white.svg",
+    }
+}
+
+# -- Customization
+# js_language = 'typescript'
+# js_source_path = '../src/**/*.ts'
+# primary_domain = 'js'
+
+myst_enable_extensions = [
+    "attrs_inline",
+]
