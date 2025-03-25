806ac64d615247e7b2020d6d31e47a79764d3306
Ross Blair <rosswilsonblair@gmail.com>
2025-03-25 15:48:54 -0500

    Update .github/scripts/scriv_check.sh
    Co-authored-by: Chris Markiewicz <markiewicz@stanford.edu>

--- .github/scripts/scriv_check.sh
+++ .github/scripts/scriv_check.sh
@@ -10,8 +10,11 @@ else
     # a changelog check is required
     # fail if the diff is empty
     if git diff --exit-code "origin/${BASE_REF}" -- "changelog.d/"; then
-        echo >&2 "User-visible changes should come with an entry in the changelog. This behavior
-can be overridden by using the \"no changelog\" label."
+        cat <<END >&2
+User-visible changes should come with an entry in the changelog.
+Create a changelog blurb with "uvx scriv create --edit".
+If no changelog entry is needed, then set the "no changelog" label.
+END
         exit 1
     fi
 fi
