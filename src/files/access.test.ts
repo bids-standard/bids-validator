a87568e721ea393568b98fc24bacd07a8d23a252
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-09-11 17:50:42 -0400

    test: Ignore file access error tests on Windows

--- src/files/access.test.ts
+++ src/files/access.test.ts
@@ -7,30 +7,34 @@ export function testAsyncFileAccess(
   fn: (file: BIDSFileDeno, ...args: any[]) => Promise<any>,
   ...args: any[]
 ) {
-  Deno.test(name, async (t) => {
-    await t.step('Dangling symlink', async () => {
-      const file = new BIDSFileDeno('tests/data', '/broken-symlink')
-      try {
-        await fn(file, ...args)
-        assert(false, 'Expected error')
-      } catch (e: any) {
-        assertObjectMatch(e, {
-          code: 'FILE_READ',
-          location: '/broken-symlink',
-        })
-        assertArrayIncludes(['NotFound', 'FilesystemLoop'], [e.subCode])
-      }
-    })
-    await t.step('Insufficient permissions', async () => {
-      const tmpfile = await Deno.makeTempFile()
-      await Deno.chmod(tmpfile, 0o000)
-      const file = new BIDSFileDeno('', tmpfile)
-      try {
-        await fn(file, ...args)
-        assert(false, 'Expected error')
-      } catch (e: any) {
-        assertObjectMatch(e, { code: 'FILE_READ', subCode: 'PermissionDenied' })
-      }
-    })
+  Deno.test({
+    name,
+    ignore: Deno.build.os === 'windows',
+    async fn(t) {
+      await t.step('Dangling symlink', async () => {
+        const file = new BIDSFileDeno('tests/data', '/broken-symlink')
+        try {
+          await fn(file, ...args)
+          assert(false, 'Expected error')
+        } catch (e: any) {
+          assertObjectMatch(e, {
+            code: 'FILE_READ',
+            location: '/broken-symlink',
+          })
+          assertArrayIncludes(['NotFound', 'FilesystemLoop'], [e.subCode])
+        }
+      })
+      await t.step('Insufficient permissions', async () => {
+        const tmpfile = await Deno.makeTempFile()
+        await Deno.chmod(tmpfile, 0o000)
+        const file = new BIDSFileDeno('', tmpfile)
+        try {
+          await fn(file, ...args)
+          assert(false, 'Expected error')
+        } catch (e: any) {
+          assertObjectMatch(e, { code: 'FILE_READ', subCode: 'PermissionDenied' })
+        }
+      })
+    },
   })
 }
