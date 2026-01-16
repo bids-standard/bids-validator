cb312e3428b47174aa63d6def8c2b23fb168a19d
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2026-01-16 09:38:40 -0500

    remove print/debug lines

--- src/bids-validator.ts
+++ src/bids-validator.ts
@@ -2,8 +2,6 @@ import { main } from './main.ts'
 import { checkAllErrors } from './summary/summary.ts'
 
 const result = await main()
-// write result to formated json file in current working directory named result_dataset_folder.json
-console.log(result.summary)
 const errors = checkAllErrors(result)
 
 if (errors.length) {
