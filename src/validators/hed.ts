78c887f9b83cbfd9454972879ebb48ff1e0bc32f
Alexander Jones <happy5214@gmail.com>
2026-09-04 09:26:21 -0500

    fix: Separately catch HED IssueError throws from file creation
    Thrown IssueErrors from HED file constructors were being caught by
    the general internal error catch, leading to unhelpful diagnostics.
    This adds an inner try-catch to separately generate useful BIDS
    issues for these (unwrapped) issues and return early.
    
    Bug: #444

--- src/validators/hed.ts
+++ src/validators/hed.ts
@@ -76,12 +76,29 @@ export async function hedValidate(
 
   try {
     const hedValidator = await import('@hed/validator')
-
     let file
-    if (context.extension === '.tsv') {
-      file = buildHedTsvFile(context, hedValidator)
-    } else {
-      file = buildHedSidecarFile(context, hedValidator)
+
+    try {
+      if (context.extension === '.tsv') {
+        file = buildHedTsvFile(context, hedValidator)
+      } else {
+        file = buildHedSidecarFile(context, hedValidator)
+      }
+    } catch (error) {
+      let issueError: Error
+      if (error instanceof Error) {
+        issueError = error
+      } else {
+        issueError = new Error('unknown error')
+      }
+      const issues = hedValidator.BidsHedIssue.fromHedIssues(
+        issueError,
+        context.file,
+      )
+      for (const issue of issues) {
+        context.dataset.issues.add(issue)
+      }
+      return
     }
 
     const hedValidationIssues = await setHedSchemas(context.dataset, hedValidator)
